4c6931fe2f58148b30d4929a1ec0d27250750067
冯经宇 <fengxingwang@fengjingyudeMacBook-Pro.local>
2019-10-17 19:28:13 +0800

    update store.js

--- monitor-ui/src/store.js
+++ monitor-ui/src/store.js
@@ -1,17 +1,19 @@
+  
 import Vue from 'vue'
 import vuex from 'vuex'
 Vue.use(vuex)
 
 export default new vuex.Store({
   state:{
-    ip: {
-      label: '',
-      value: ''
-    }
+    ip: {},
+    tableExtendActive:-1,//table组件扩展状态
   },
   mutations:{
     storeip (state, ip) {
       state.ip = ip    
+    },
+    changeTableExtendActive(state,index){
+      state.tableExtendActive = index
     }
   }
 })
