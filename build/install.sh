0deb82c3179c45de0fe7de191402c59d6f849dab
Jun Chen <junchensz@foxmail.com>
2019-10-22 16:03:16 +0800

    #109 support config and remote host parameters

--- build/install.sh
+++ build/install.sh
@@ -12,6 +12,12 @@ then
     exit 1
 fi
 
+if [ $# -eq 2 ]
+then
+  export config=$1
+  export remote_host=$2
+fi
+
 mkdir -p /app/docker/prometheus
 mkdir -p /app/docker/prometheus/rules
 mkdir -p /app/docker/alertmanager
@@ -25,7 +31,11 @@ cp ../monitor-server/conf/docker/prometheus.yml /app/docker/prometheus
 cp ../monitor-server/conf/docker/alertmanager.yml /app/docker/alertmanager
 cp ../monitor-server/conf/docker/monitor.json /app/docker/monitor/conf/default.json
 
-source monitor.cfg
+if [ -z ${config} ];then
+  source monitor.cfg
+else
+  source ${config}
+fi
 
 sed "s~{{MONITOR_DATABASE_IMAGE_NAME}}~$database_image_name~g" docker-compose.tpl > docker-compose.yml
 sed -i "s~{{MYSQL_ROOT_PASSWORD}}~$database_init_password~g" docker-compose.yml
@@ -37,11 +47,11 @@ sed -i "s~{{MONITOR_SERVER_PORT}}~$monitor_server_port~g" /app/docker/monitor/co
 
 sed -i "s~{{MONITOR_SERVER_PORT}}~$monitor_server_port~g" /app/docker/alertmanager/alertmanager.yml
 
-if [ $# -ge 1 ]
+if [ -z ${remote_host} ]
 then
-  docker-compose -f docker-compose.yml -H $1 up -d
-else
   docker-compose  -f docker-compose.yml  up -d
+else
+  docker-compose -f docker-compose.yml -H $1 up -d
 fi
 
  
