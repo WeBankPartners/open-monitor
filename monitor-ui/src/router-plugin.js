4ceca1df59f1e63e96a73bfc8f0a72c20b668a23
pobu168 <244202969@qq.com>
2019-11-07 16:05:13 +0800

    #217 Plug-in transformation

--- monitor-ui/src/router-plugin.js
+++ monitor-ui/src/router-plugin.js
@@ -14,60 +14,42 @@ import viewConfigIndex from '@/components/pages/DIY-view/view-config-index'
 import viewConfig from '@/components/pages/DIY-view/view-config'
 import editView from '@/components/pages/DIY-view/edit-view'
 import searchHomepage from '@/components/pages/Search-homepage'
-import test from '@/components/test'
 import index from '@/components/index'
 
 Vue.use(Router)
-
-
-const router = new Router({
-    routes: [
-      {
-        path: '/',
-        name: 'index',
-        component: index,
-        redirect: '/portal',
-        title: '测试首页',
-        children: [
-          { path: 'alarmManagement',  name: 'alarmManagement', title: '告警管理', meta: {},
-           component: alarmManagement },
-          { path: 'portal',  name: 'portal', title: '首页', meta: {},
-           component: portal },
-          { path: 'mainView',  name: 'mainView', title: '对象监控', meta: {},
-           component: mainView },
-          { path: 'monitorConfigIndex',  name: 'monitorConfigIndex', title: '', meta: {},
-            component: monitorConfigIndex,
-            redirect: 'endpointManagement', 
-            children: [
-              { path: 'endpointManagement',  name: 'endpointManagement', title: '对象管理', meta: {},
-              component: endpointManagement },
-              { path: 'groupManagement',  name: 'groupManagement', title: '组管理', meta: {},
-              component: groupManagement },
-              { path: 'thresholdManagement',  name: 'thresholdManagement', title: '阀值配置', meta: {},
-              component: thresholdManagement },
-              { path: 'logManagement',  name: 'logManagement', title: '阀值配置', meta: {},
-              component: logManagement },
-            ]
-          },
-          { path: 'metricConfig',  name: 'metricConfig', title: '视图配置', meta: {},
-           component: metricConfig },
-          { path: 'viewConfigIndex',  name: 'viewConfigIndex', title: '自定义视图主页', meta: {},
-           component: viewConfigIndex },
-          { path: 'viewConfig',  name: 'viewConfig', title: '自定义视图', meta: {},
-           component: viewConfig },
-          { path: 'editView',  name: 'editView', title: '自定义视图编辑', meta: {},
-           component: editView },
-           { path: 'searchHomepage',  name: 'searchHomepage', title: '搜索主页', meta: {},
-           component: searchHomepage }
-        ]
-      },
-      {
-        path: '/test',
-        name: 'test',
-        component: test,
-        title: 'test'
-      }
-    ]  
-}) 
+const router = [
+  { path: 'index',  name: 'index', title: '首页', meta: {},
+    component: index },
+  { path: 'alarmManagement',  name: 'alarmManagement', title: '告警管理', meta: {},
+    component: alarmManagement },
+  { path: 'portal',  name: 'portal', title: '首页', meta: {},
+    component: portal },
+  { path: 'mainView',  name: 'mainView', title: '对象监控', meta: {},
+    component: mainView },
+  { path: 'monitorConfigIndex',  name: 'monitorConfigIndex', title: '', meta: {},
+    component: monitorConfigIndex,
+    redirect: 'monitorConfigIndex/endpointManagement', 
+    children: [
+      { path: 'endpointManagement',  name: 'endpointManagement', title: '对象管理', meta: {},
+      component: endpointManagement },
+      { path: 'groupManagement',  name: 'groupManagement', title: '组管理', meta: {},
+      component: groupManagement },
+      { path: 'thresholdManagement',  name: 'thresholdManagement', title: '阀值配置', meta: {},
+      component: thresholdManagement },
+      { path: 'logManagement',  name: 'logManagement', title: '阀值配置', meta: {},
+      component: logManagement },
+    ]
+  },
+  { path: 'metricConfig',  name: 'metricConfig', title: '视图配置', meta: {},
+    component: metricConfig },
+  { path: 'viewConfigIndex',  name: 'viewConfigIndex', title: '自定义视图主页', meta: {},
+    component: viewConfigIndex },
+  { path: 'viewConfig',  name: 'viewConfig', title: '自定义视图', meta: {},
+    component: viewConfig },
+  { path: 'editView',  name: 'editView', title: '自定义视图编辑', meta: {},
+    component: editView },
+    { path: 'searchHomepage',  name: 'searchHomepage', title: '搜索主页', meta: {},
+    component: searchHomepage }
+]
 
 export default router
