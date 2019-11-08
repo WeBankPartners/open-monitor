905e50a63eca463446554eec3c987a08020bd257
pobu168 <244202969@qq.com>
2019-11-08 11:35:56 +0800

    Update files

--- monitor-ui/src/components/index.vue
+++ monitor-ui/src/components/index.vue
@@ -8,31 +8,30 @@
 </template>
 
 <script>
-import Menu from './sub-c/menu'
+import Menu from "./sub-c/menu";
 export default {
-  name: 'index',
-  methods: {
-  },
-  components:{
+  name: "index",
+  methods: {},
+  components: {
     Menu
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
-  @import '../assets/css/common.less';
-  .index {
-    min-width: 1200px;
-    background: white;
-  }
-  .content {
-    background-image: url('../assets/img/watermark_wecube_monitor.png');
-    // width:1200px;
-    min-height: 95vh;
-    height: 100%;
-    margin: 0 auto;
-    padding: 10px;
-    // background:white;
-  }
+@import "../assets/css/common.less";
+.index {
+  min-width: 1200px;
+  background: white;
+}
+.content {
+  background-image: url("../assets/img/watermark_wecube_monitor.png");
+  // width:1200px;
+  min-height: 95vh;
+  height: 100%;
+  margin: 0 auto;
+  padding: 10px;
+  // background:white;
+}
 </style>
