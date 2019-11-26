753c620dc09eaa4885813f5265ae1333c6315f9d
pobu168 <244202969@qq.com>
2019-11-26 10:03:55 +0800

    Clean up obsolete code

--- monitor-ui/src/components/pages/system-monitoring/system-monitoring.vue
+++ monitor-ui/src/components/pages/system-monitoring/system-monitoring.vue
@@ -184,57 +184,10 @@ export default {
         })) 
       })
       this.$httpRequestEntrance.httpRequestEntrance('GET',this.apiCenter.metricConfigView.api, {config: `[${params.join(',')}]`}, responseData => {
-        // var legend = []
-        // if (responseData.series.length === 0) {
-        //   this.noDataTip = true
-        //   return
-        // }
-        // const colorX = ['#339933','#33CCCC','#666699','#66CC66','#996633','#9999CC','#339966','#663333','#6666CC','#336699','#3399CC','#33CC66','#CC3333','#CC6666','#996699','#CC9933']
-        // let colorSet = []
-        // for (let i=0;i<colorX.length;i++) {
-        //   let tmpIndex = viewIndex*3 + i
-        //   tmpIndex = tmpIndex%colorX.length
-        //   colorSet.push(colorX[tmpIndex])
-        // }
-        // responseData.series.forEach((item,index)=>{
-        //   legend.push(item.name)
-        //   item.symbol = 'none'
-        //   item.smooth = true
-        //   item.lineStyle = {
-        //     width: 1
-        //   }
-        //   item.itemStyle = {
-        //     normal:{
-        //       color: colorSet[index]
-        //     }
-        //   }
-        //   item.areaStyle = {
-        //     normal: {
-        //       color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-        //           offset: 0,
-        //           color: colorSet[index]
-        //       }, {
-        //           offset: 1,
-        //           color: 'white'
-        //       }])
-        //     }
-        //   }
-        // })
-        // let config = {}
-        // config = {
-        //   title: responseData.title,
-        //   legend: legend,
-        //   series: responseData.series,
-        //   yaxis: responseData.yaxis
-        // }
-        // this.elId = id
-        // drawChart(this, config, {eye: false,dataZoom:false,clear:true})
-
+       
         this.elId = id
         const chartConfig = {eye: false,dataZoom:false,clear:true}
-
         readyToDraw(this,responseData, viewIndex, chartConfig)
-
       })
     },
     resizeEvent: function(i, newH, newW, newHPx, newWPx){
