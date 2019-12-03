0eac87c0b8a7fcf57706c4d4578302ad5c6c4298
Jingyu FENG <244202969@qq.com>
2019-12-03 15:06:32 +0800

    Update axiosHttp.js

--- monitor-ui/src/assets/js/axiosHttp.js
+++ monitor-ui/src/assets/js/axiosHttp.js
@@ -9,14 +9,14 @@ export default function ajax (options) {
     baseURL: baseURL,
     url: options.url,
     timeout: 30000,
-    params: options.params,
+    params: encodeURI(JSON.stringify(options.params)),
     // params: options.params || '',
     headers: {
       'Content-type': 'application/json;charset=UTF-8',
       // 'X-Auth-Token': cookies.getAuthorization() || null
     },
     // data: JSON.stringify(options.data || '')
-    data: JSON.stringify(options.data)
+    data: encodeURI(JSON.stringify(options.data))
   }
   // 导出请求时增加响应类型
   if (options.url.includes('/export/')) {
