3c2b0d706d63ac59bf1c152425a0cc65731048a3
zgyzgyhero <894526647@qq.com>
2019-12-10 16:57:21 +0800

    Add business log monitor

--- monitor-server/api/v1/alarm/business.go
+++ monitor-server/api/v1/alarm/business.go
@@ -1,39 +1,39 @@
 package alarm
 
 import (
+	mid "github.com/WeBankPartners/open-monitor/monitor-server/middleware"
+	m "github.com/WeBankPartners/open-monitor/monitor-server/models"
 	"github.com/gin-gonic/gin"
 	"strconv"
-	mid "github.com/WeBankPartners/open-monitor/monitor-server/middleware"
 	"fmt"
 	"github.com/WeBankPartners/open-monitor/monitor-server/services/db"
-	m "github.com/WeBankPartners/open-monitor/monitor-server/models"
-	"net/http"
 	"encoding/json"
+	"net/http"
 	"strings"
 )
 
-func GetEndpointProcessConfig(c *gin.Context)  {
+func GetEndpointBusinessConfig(c *gin.Context)  {
 	endpointId,err := strconv.Atoi(c.Query("id"))
 	if err != nil || endpointId <= 0 {
 		mid.ReturnValidateFail(c, fmt.Sprintf("Param id validate fail %v", err))
 		return
 	}
-	err,data := db.GetProcessList(endpointId)
+	err,data := db.GetBusinessList(endpointId)
 	if err != nil {
-		mid.ReturnError(c, "Get process list fail", err)
+		mid.ReturnError(c, "Get business list fail", err)
 	}else{
 		mid.ReturnData(c, data)
 	}
 }
 
-func UpdateEndpointProcessConfig(c *gin.Context)  {
-	var param m.ProcessUpdateDto
+func UpdateEndpointBusinessConfig(c *gin.Context)  {
+	var param m.BusinessUpdateDto
 	if err := c.ShouldBindJSON(&param); err==nil {
-		err = db.UpdateProcess(param)
+		err = db.UpdateBusiness(param)
 		if err != nil {
-			mid.ReturnError(c, "Update process fail ", err)
+			mid.ReturnError(c, "Update business fail ", err)
 		}else{
-			err = updateNodeExporterProcessConfig(param.EndpointId)
+			err = updateNodeExporterBusinessConfig(param.EndpointId)
 			if err != nil {
 				mid.ReturnError(c, "Update node exporter config fail ", err)
 				return
@@ -45,12 +45,12 @@ func UpdateEndpointProcessConfig(c *gin.Context)  {
 	}
 }
 
-type processHttpDto struct {
-	Process  []string  `json:"process"`
+type businessHttpDto struct {
+	Paths  []string  `json:"paths"`
 }
 
-func updateNodeExporterProcessConfig(endpointId int) error {
-	err,data := db.GetProcessList(endpointId)
+func updateNodeExporterBusinessConfig(endpointId int) error {
+	err,data := db.GetBusinessList(endpointId)
 	if err != nil {
 		mid.LogError("Update node_exporter fail ", err)
 		return err
@@ -61,16 +61,16 @@ func updateNodeExporterProcessConfig(endpointId int) error {
 		mid.LogError("Update node_exporter fail, get endpoint msg fail ", err)
 		return err
 	}
-	postParam := processHttpDto{Process:[]string{}}
+	postParam := businessHttpDto{Paths:[]string{}}
 	for _,v := range data {
-		postParam.Process = append(postParam.Process, v.Name)
+		postParam.Paths = append(postParam.Paths, v.Path)
 	}
 	postData,err := json.Marshal(postParam)
 	if err != nil {
 		mid.LogError("Update node_exporter fail, marshal post data fail ", err)
 		return err
 	}
-	url := fmt.Sprintf("http://%s/process/config", endpointObj.Address)
+	url := fmt.Sprintf("http://%s/business/config", endpointObj.Address)
 	resp, err := http.Post(url, "application/json", strings.NewReader(string(postData)))
 	if err != nil {
 		mid.LogError("Update node_exporter fail, http post fail ", err)
