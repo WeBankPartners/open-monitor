90d06d1522594b7229caf2cc1f181ad49e4bbb16
pobu168 <244202969@qq.com>
2019-12-20 11:40:08 +0800

    #322 Add login

--- monitor-ui/src/App.vue
+++ monitor-ui/src/App.vue
@@ -18,5 +18,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   line-height: inherit;
   font-size: 12px;
+
+  width: 100%;
+  height: 100%;
 }
+
+html,body{
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+
 </style>
