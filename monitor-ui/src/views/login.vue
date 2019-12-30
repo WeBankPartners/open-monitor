46342a8959786be2f5522a681d99b99c52dc7245
pobu168 <244202969@qq.com>
2019-12-30 16:40:00 +0800

    #351 Add register

--- monitor-ui/src/views/login.vue
+++ monitor-ui/src/views/login.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="login">
-    <div class="login-con">
-      <Card icon="log-in" title="欢迎登录" :bordered="false">
-        <div class="form-con">
-          <Form ref="loginForm" :model="form" :rules="rules" @keydown.enter.native="handleSubmit">
-            <FormItem prop="userName">
-              <Input v-model="form.userName" placeholder="请输入用户名">
-                <span slot="prepend">
-                  <Icon :size="16" type="ios-person"></Icon>
-                </span>
-              </Input>
-            </FormItem>
-            <FormItem prop="password">
-              <Input type="password" v-model="form.password" placeholder="请输入密码">
-                <span slot="prepend">
-                  <Icon :size="14" type="md-lock"></Icon>
-                </span>
-              </Input>
-            </FormItem>
-            <FormItem>
-              <Button @click="handleSubmit" type="primary" long>登录</Button>
-            </FormItem>
-          </Form>
+    <Row>
+      <Col span="6" offset="9">
+        <div class="login-con">
+          <Card icon="log-in" title="欢迎登录" :bordered="false">
+            <div class="form-con">
+              <Form ref="loginForm" :model="form" :rules="rules" @keydown.enter.native="handleSubmit">
+                <FormItem prop="userName">
+                  <Input v-model="form.userName" placeholder="请输入用户名">
+                    <span slot="prepend">
+                      <Icon :size="16" type="ios-person"></Icon>
+                    </span>
+                  </Input>
+                </FormItem>
+                <FormItem prop="password">
+                  <Input type="password" v-model="form.password" placeholder="请输入密码">
+                    <span slot="prepend">
+                      <Icon :size="14" type="md-lock"></Icon>
+                    </span>
+                  </Input>
+                </FormItem>
+                <FormItem>
+                  <Button @click="handleSubmit" type="primary" long>登录</Button>
+                </FormItem>
+              </Form>
+            </div>
+          </Card>
         </div>
-      </Card>
-    </div>
+      </Col>
+    </Row>
   </div>
 </template>
 
@@ -63,12 +67,12 @@ export default {
       }).then((response) => {
           if (response.status < 400) {
             cookies.setAuthorization(`${response.data.data.token}`)
-            localStorage.username = response.data.user
-            this.$Message.success('已登入!')
+            localStorage.username = response.data.data.user
+            this.$Message.success(response.data.msg)
             this.$router.push({path: '/'})
           }
       })
-      .catch((error) => {
+      .catch(() => {
         this.$Message.warning(this.$t('tips.failed'))
       });
     }
@@ -78,32 +82,16 @@ export default {
 
 <style lang="less" scope>
 .login{
-    width: 100%;
-    height: 100%;
-    background-image: url('../assets/img/login-bg.jpg');
-    background-size: cover;
-    background-position: center;
-    position: relative;
-    &-con{
-        position: absolute;
-        right: 39%;
-        top: 50%;
-        transform: translateY(-60%);
-        width: 300px;
-        &-header{
-            font-size: 16px;
-            font-weight: 300;
-            text-align: center;
-            padding: 30px 0;
-        }
-        .form-con{
-            padding: 10px 0 0;
-        }
-        .login-tip{
-            font-size: 10px;
-            text-align: center;
-            color: #c3c3c3;
-        }
-    }
+  width: 100%;
+  height: 100%;
+  background-image: url('../assets/img/login-bg.jpg');
+  background-size: cover;
+  background-position: center;
+  &-con {
+    margin-top: 40%;
+  }
+  .form-con{
+    padding: 10px 0 0;
+  }
 }
 </style>
