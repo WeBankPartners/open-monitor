877bab0d310c2196ee762d9a331aaaa87af6d470
pobu168 <244202969@qq.com>
2020-01-07 16:27:34 +0800

    #352 Add user information modify

---
+++ monitor-ui/src/views/user-management/role-management.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class=" ">
+      角色管理
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+
+    }
+  },
+  methods: {},
+  components: {},
+}
+</script>
+
+<style scoped lang="less">
+</style>
