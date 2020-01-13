5d5edf1567f51de8dcf75a3cee6c1a87499448b9
zgyzgyhero <894526647@qq.com>
2020-01-13 16:33:13 +0800

    Add trans gateway config

--- build/plugin/start.sh
+++ build/plugin/start.sh
@@ -21,6 +21,10 @@ nohup ./prometheus --config.file=prometheus.yml --web.enable-lifecycle --storage
 cd ../agent_manager/
 mkdir -p logs
 nohup ./agent_manager > logs/app.log 2>&1 &
+cd ../transgateway/
+mkdir -p logs
+mkdir -p data
+nohup ./transgateway > logs/app.log 2>&1 &
 cd ../monitor/
 mkdir -p logs
 ./monitor-server --export_agent=true
