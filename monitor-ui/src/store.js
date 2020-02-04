925e07dcd13bcfbd023a9786a9b566cd2d5c9f16
pobu168 <244202969@qq.com>
2020-02-04 15:49:50 +0800

    #402 Enhance recursive view switch

--- monitor-ui/src/store.js
+++ monitor-ui/src/store.js
@@ -6,13 +6,23 @@ export default new vuex.Store({
   state: {
     ip: {},
     tableExtendActive: -1, //table组件扩展状态
+    recursiveNum: []
   },
   mutations: {
     storeip(state, ip) {
-      state.ip = ip;
+      state.ip = ip
     },
     changeTableExtendActive(state, index) {
-      state.tableExtendActive = index;
-    }
+      state.tableExtendActive = index
+    },
+    addRecursiveNum(state, index) {
+      state.recursiveNum.push(index)
+    },
+    removeRecursiveNum(state, index) {
+      state.recursiveNum.splice(index, 1)
+    },
+    emptyRecursiveNum(state) {
+      state.recursiveNum = []
+    },
   }
 });
