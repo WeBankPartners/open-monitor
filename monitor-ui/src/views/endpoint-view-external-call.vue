3650b1338d8206c4955f0ca7f77ba20e08798b9f
pobu168 <244202969@qq.com>
2020-03-30 15:45:58 +0800

    #459 Add call response

---
+++ monitor-ui/src/views/endpoint-view-external-call.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="page" id="endpointView">
+    <Title :title="$t('menu.endpointView')"></Title>
+    <SearchExternal ref="search" />
+    <Charts v-if="showCharts" :charts='charts' ref="parentCharts" />
+    <div v-if="recursiveViewConfig.length && showRecursive">
+      <recursive :recursiveViewConfig="recursiveViewConfig" :params="params"></recursive>
+    </div>
+  </div>
+</template>
+<script>
+import SearchExternal from '@/components/search-external'
+import Charts from '@/components/charts'
+import recursive from '@/views/recursive-view/recursive'
+export default {
+  name: 'endpoint-view',
+  data() {
+    return {
+      showCharts: false,
+      showRecursive: false,
+      params: null,
+      charts: {
+        chartsConfig: []
+      },
+      recursiveViewConfig: []
+    }
+  },
+  mounted() {
+    this.$refs.search.getChartsConfig() 
+  },
+  methods: {
+    manageCharts (chartsConfig, params) {
+      if (params.sys) {
+        this.params = params
+        this.showCharts = false
+        this.recursiveView(params)
+        return
+      }
+      this.charts.chartsConfig = []
+      chartsConfig.forEach(item => {
+        item.autoRefresh = params.autoRefresh
+        let chart = {
+          tabTape: {
+            label: item.title,
+            name: item.title + '_',
+          },
+          btns: item.tags.option,
+          tagsUrl: item.tags.url,
+          charts: item.charts,
+          params: params
+        }
+        this.charts.chartsConfig.push(chart)
+      })
+      this.showCharts = true
+      this.showRecursive = false
+      this.$refs.parentCharts.refreshCharts()
+    },
+    recursiveView (params) {
+      this.recursiveViewConfig = []
+      this.$root.$httpRequestEntrance.httpRequestEntrance('GET',this.$root.apiCenter.recursive.api, params, responseData => {
+        this.showRecursive = true
+        this.recursiveViewConfig = [responseData]
+      })
+    }
+  },
+  components: {
+    SearchExternal,
+    Charts,
+    recursive
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.page {
+  margin: 16px;
+}
+</style>
