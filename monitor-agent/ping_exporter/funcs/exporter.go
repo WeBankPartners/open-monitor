b2c1a0929bf32cc97c3227021fa095d7a12d98e0
zgyzgyhero <894526647@qq.com>
2020-04-08 19:50:27 +0800

    Update ping exporter

--- monitor-agent/ping_exporter/funcs/exporter.go
+++ monitor-agent/ping_exporter/funcs/exporter.go
@@ -7,43 +7,71 @@ import (
 	"sort"
 )
 
-type exportMetricObj struct {
+type exportPingMetricObj struct {
 	Ip  string
 	Value  int
 	Note  string
 }
 
+type exportTelnetMetricObj struct {
+	Ip string
+	Port int
+	Value  int
+	Note  string
+}
+
 var (
-	exportMetrics = make(map[string]*exportMetricObj)
-	exportLock = new(sync.RWMutex)
+	exportPingMetrics = make(map[string]*exportPingMetricObj)
+	exportTelnetMetrics = make(map[string]*exportTelnetMetricObj)
+	exportPingLock = new(sync.RWMutex)
+	exportTelnetLock = new(sync.RWMutex)
 )
 
-func UpdateExportMetric(result map[string]int,successCount int)  {
-	exportLock.Lock()
-	exportMetrics = make(map[string]*exportMetricObj)
+func UpdatePingExportMetric(result map[string]int,successCount int)  {
+	exportPingLock.Lock()
+	exportPingMetrics = make(map[string]*exportPingMetricObj)
 	for k,v := range result {
-		exportMetrics[k] = &exportMetricObj{Value:v, Note:fmt.Sprintf("# HELP target ip %s \r", k)}
+		exportPingMetrics[k] = &exportPingMetricObj{Value:v, Note:fmt.Sprintf("# HELP target ip %s \r", k)}
 	}
-	exportMetrics[Config().Metrics.CountNum] = &exportMetricObj{Value:len(result), Note:"# HELP task ip num \r"}
-	exportMetrics[Config().Metrics.CountSuccess] = &exportMetricObj{Value:successCount, Note:"# HELP alive ip num \r"}
-	exportMetrics[Config().Metrics.CountFail] = &exportMetricObj{Value:len(result)-successCount, Note:"# HELP dead ip num \r"}
-	exportLock.Unlock()
+	exportPingMetrics[Config().Metrics.PingCountNum] = &exportPingMetricObj{Value:len(result), Note:"# HELP task ip num \r"}
+	exportPingMetrics[Config().Metrics.PingCountSuccess] = &exportPingMetricObj{Value:successCount, Note:"# HELP alive ip num \r"}
+	exportPingMetrics[Config().Metrics.PingCountFail] = &exportPingMetricObj{Value:len(result)-successCount, Note:"# HELP dead ip num \r"}
+	exportPingLock.Unlock()
+}
+
+func UpdateTelnetExportMetric(result []*TelnetObj)  {
+	exportTelnetLock.Lock()
+	exportTelnetMetrics = make(map[string]*exportTelnetMetricObj)
+	for _,v := range result {
+		tmpValue := 1
+		if v.Success {
+			tmpValue = 0
+		}
+		exportTelnetMetrics[fmt.Sprintf("%s_%d", v.Ip, v.Port)] = &exportTelnetMetricObj{Ip:v.Ip, Port:v.Port, Value:tmpValue, Note:fmt.Sprintf("# HELP target ip %s port %d \r", v.Ip, v.Port)}
+	}
+	exportTelnetLock.Unlock()
 }
 
 func GetExportMetric() []byte {
+	pingByte := getPingExportMetric()
+	telnetByte := getTelnetExportMetric()
+	return append(pingByte, telnetByte...)
+}
+
+func getPingExportMetric() []byte {
 	var buff bytes.Buffer
 	buff.WriteString("# HELP ping check 0 -> alive, 1 -> dead, 2 -> problem.")
-	metricString := Config().Metrics.Default
+	metricString := &Config().Metrics.Ping
 	var tmpExportMetric exportMetricList
-	exportLock.RLock()
-	for k,v := range exportMetrics {
-		tmpExportMetric = append(tmpExportMetric, &exportMetricObj{Ip:k, Value:v.Value, Note:v.Note})
+	exportPingLock.RLock()
+	for k,v := range exportPingMetrics {
+		tmpExportMetric = append(tmpExportMetric, &exportPingMetricObj{Ip:k, Value:v.Value, Note:v.Note})
 	}
-	exportLock.RUnlock()
+	exportPingLock.RUnlock()
 	sort.Sort(tmpExportMetric)
 	for _,v := range tmpExportMetric {
 		buff.WriteString(v.Note)
-		if v.Ip == Config().Metrics.CountNum || v.Ip == Config().Metrics.CountSuccess || v.Ip == Config().Metrics.CountFail {
+		if v.Ip == Config().Metrics.PingCountNum || v.Ip == Config().Metrics.PingCountSuccess || v.Ip == Config().Metrics.PingCountFail {
 			buff.WriteString(fmt.Sprintf("%s %d \r\n", v.Ip, v.Value))
 			continue
 		}
@@ -52,7 +80,14 @@ func GetExportMetric() []byte {
 	return buff.Bytes()
 }
 
-type exportMetricList []*exportMetricObj
+func getTelnetExportMetric() []byte {
+	var buff bytes.Buffer
+	buff.WriteString("# HELP telnet check 0 -> alive, 1 -> dead \r")
+
+	return buff.Bytes()
+}
+
+type exportMetricList []*exportPingMetricObj
 
 func (p exportMetricList) Len() int {
 	return len(p)
