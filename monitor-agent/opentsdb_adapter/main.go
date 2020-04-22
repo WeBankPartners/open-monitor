5d91878acef3e410136483e8770185d6dbb86f3e
zgyzgyhero <894526647@qq.com>
2020-04-22 14:25:45 +0800

    Add OpenTsdb read

--- monitor-agent/opentsdb_adapter/main.go
+++ monitor-agent/opentsdb_adapter/main.go
@@ -7,7 +7,8 @@ import (
 
 func main()  {
 	openTsdbUrl := flag.String("u", "http://127.0.0.1:4242", "openTsdb url")
+	listenPort := flag.Int("p", 9202, "listening port")
 	flag.Parse()
 	funcs.OpenTSDBUrl = *openTsdbUrl
-	funcs.InitHttpServer()
+	funcs.InitHttpServer(*listenPort)
 }
