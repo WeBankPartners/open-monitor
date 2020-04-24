ac86a2bce2aeb5d879c35dd7b7268189a00373f8
pobu168 <244202969@qq.com>
2020-04-24 17:00:08 +0800

    #558 Enhance custom view display

--- monitor-ui/src/assets/js/gridUtils.js
+++ monitor-ui/src/assets/js/gridUtils.js
@@ -5,8 +5,9 @@ export const resizeEvent = function(that, i, newH, newW, newHPx, newWPx){
     if (item.i === i) {
       that.layoutData[index].h = newH
       that.layoutData[index].w = newW
+      that.layoutData[index].activeCharts[0].style = ''
       var myChart = echarts.init(document.getElementById(item.id))
-      myChart.resize({height:newHPx - 34+'px',width:newWPx - 6 +'px'})
+      myChart.resize({height:newHPx - 50+'px',width:newWPx - 6 +'px'})
       return
     }
   })
