0285aa9015edb9a24a2b611f7f67e223c8d3efd3
zgyzgyhero <894526647@qq.com>
2020-05-13 15:53:33 +0800

    Update service discover

--- monitor-agent/agent_manager/api/api.go
+++ monitor-agent/agent_manager/api/api.go
@@ -5,11 +5,14 @@ import (
 	"net/http"
 	"fmt"
 	"github.com/WeBankPartners/open-monitor/monitor-agent/agent_manager/funcs"
+	"log"
 )
 
 func InitHttpServer() {
 	http.Handle("/deploy/add", http.HandlerFunc(manager.AddDeploy))
 	http.Handle("/deploy/delete", http.HandlerFunc(manager.DelDeploy))
 	http.Handle("/process/list", http.HandlerFunc(manager.DisplayProcess))
+	http.Handle("/deploy/init", http.HandlerFunc(manager.InitDeploy))
+	log.Printf("start to listen : %d ..... ", funcs.Config().Http.Port)
 	http.ListenAndServe(fmt.Sprintf(":%d", funcs.Config().Http.Port), nil)
 }
