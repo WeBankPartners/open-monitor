1a832d6f608aa164a72e025f583a5f84c45a4bde
zgyzgyhero <894526647@qq.com>
2020-05-22 11:19:01 +0800

    Update core token

--- monitor-server/models/const.go
+++ monitor-server/models/const.go
@@ -4,6 +4,6 @@ const (
 	ServerToken = `default-token-used-in-server-side`
 	DatetimeFormat = `2006-01-02 15:04:05`
 	Version = `1.0.0`
-	TmpCoreToken = `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJTWVNfTU9OSVRPUiIsImlhdCI6MTU5MDExNTg3NSwidHlwZSI6ImFjY2Vzc1Rva2VuIiwiY2xpZW50VHlwZSI6IlNVQl9TWVNURU0iLCJleHAiOjE3NDU2MzU4NzUsImF1dGhvcml0eSI6IltTVUJfU1lTVEVNXSJ9.0SkxMnZgbhH-GaKYFFiJDjjQWfDB9E5IZ4xsd2jOrT3HumLfJYlEtIFGTm683PhVnindSrbv8FFt3No4YONs_A`
+	TmpCoreToken = `***REMOVED***`
 
 )
