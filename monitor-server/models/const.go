a971e05033fbbaa9b56271188023f53af168d934
zgyzgyhero <894526647@qq.com>
2020-05-26 16:04:37 +0800

    Update chart compare

--- monitor-server/models/const.go
+++ monitor-server/models/const.go
@@ -3,6 +3,7 @@ package models
 const (
 	ServerToken = `default-token-used-in-server-side`
 	DatetimeFormat = `2006-01-02 15:04:05`
+	DateFormatWithZone = `2006-01-02 15:04:05 MST`
 	Version = `1.0.0`
 	TmpCoreToken = `***REMOVED***`
 
