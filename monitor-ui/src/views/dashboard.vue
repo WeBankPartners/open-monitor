2f6fdbfa334538d24f64acf77a5f5717f9df341d
pobu168 <244202969@qq.com>
2020-06-30 11:50:40 +0800

    Upgrade homepage

--- monitor-ui/src/views/dashboard.vue
+++ monitor-ui/src/views/dashboard.vue
@@ -1,67 +1,73 @@
 <template>
   <div>
-    <header>
-      <div class="search-zone">
-        <span class="params-title">{{$t('field.relativeTime')}}：</span>
-        <Select v-model="viewCondition.timeTnterval" :disabled="disableTime" style="width:80px"  @on-change="initPanals">
-          <Option v-for="item in dataPick" :value="item.value" :key="item.value">{{ item.label }}</Option>
-        </Select>
+    <section v-if="isPlugin">
+      <div style="margin: 100px 0;text-align: center;font-size:14px">
+        {{$t('tips.dashboardEmpty')}}
       </div>
-      <div class="search-zone">
-        <span class="params-title">{{$t('placeholder.refresh')}}：</span>
-        <Select v-model="viewCondition.autoRefresh" :disabled="disableTime" style="width:100px" @on-change="initPanals" :placeholder="$t('placeholder.refresh')">
-          <Option v-for="item in autoRefreshConfig" :value="item.value" :key="item.value">{{ item.label }}</Option>
-        </Select>
-      </div>
-      <div class="search-zone">
-        <span class="params-title">{{$t('field.timeInterval')}}：</span>
-        <DatePicker 
-          type="datetimerange" 
-          :value="viewCondition.dateRange" 
-          format="yyyy-MM-dd HH:mm:ss" 
-          placement="bottom-start" 
-          @on-change="datePick" 
-          :placeholder="$t('placeholder.datePicker')" 
-          style="width: 320px">
-        </DatePicker>
-      </div>
-    </header>
-    <div>
-      <grid-layout
-        :layout.sync="layoutData"
-        :col-num="12"
-        :row-height="30"
-        :is-draggable="false"
-        :is-resizable="false"
-        :is-mirrored="false"
-        :vertical-compact="true"
-        :use-css-transforms="true"
-        >
-        <grid-item v-for="(item,index) in layoutData"
-          class="c-dark"
-          :x="item.x"
-          :y="item.y"
-          :w="item.w"
-          :h="item.h"
-          :i="item.i"
-          :key="index"
-          @resized="resizeEvent">   
-          <div class="c-dark" style="display:flex;justify-content:flex-end;padding:0 32px;">
-            <div class="header-grid header-grid-name">
-              {{item.i}}
+    </section>
+    <section v-else>
+      <header>
+        <div class="search-zone">
+          <span class="params-title">{{$t('field.relativeTime')}}：</span>
+          <Select v-model="viewCondition.timeTnterval" :disabled="disableTime" style="width:80px"  @on-change="initPanals">
+            <Option v-for="item in dataPick" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
+        </div>
+        <div class="search-zone">
+          <span class="params-title">{{$t('placeholder.refresh')}}：</span>
+          <Select v-model="viewCondition.autoRefresh" :disabled="disableTime" style="width:100px" @on-change="initPanals" :placeholder="$t('placeholder.refresh')">
+            <Option v-for="item in autoRefreshConfig" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
+        </div>
+        <div class="search-zone">
+          <span class="params-title">{{$t('field.timeInterval')}}：</span>
+          <DatePicker 
+            type="datetimerange" 
+            :value="viewCondition.dateRange" 
+            format="yyyy-MM-dd HH:mm:ss" 
+            placement="bottom-start" 
+            @on-change="datePick" 
+            :placeholder="$t('placeholder.datePicker')" 
+            style="width: 320px">
+          </DatePicker>
+        </div>
+      </header>
+      <div>
+        <grid-layout
+          :layout.sync="layoutData"
+          :col-num="12"
+          :row-height="30"
+          :is-draggable="false"
+          :is-resizable="false"
+          :is-mirrored="false"
+          :vertical-compact="true"
+          :use-css-transforms="true"
+          >
+          <grid-item v-for="(item,index) in layoutData"
+            class="c-dark"
+            :x="item.x"
+            :y="item.y"
+            :w="item.w"
+            :h="item.h"
+            :i="item.i"
+            :key="index"
+            @resized="resizeEvent">   
+            <div class="c-dark" style="display:flex;justify-content:flex-end;padding:0 32px;">
+              <div class="header-grid header-grid-name">
+                {{item.i}}
+              </div>
             </div>
-          </div>
-          <section>
-            <div v-for="(chartInfo,chartIndex) in item._activeCharts" :key="chartIndex">
-              <CustomChart v-if="['line','bar'].includes(chartInfo.chartType)" :chartInfo="chartInfo" :chartIndex="index" :params="viewCondition"></CustomChart>
-              <CustomPieChart v-if="chartInfo.chartType === 'pie'" :chartInfo="chartInfo" :chartIndex="index" :params="viewCondition"></CustomPieChart>
-            </div>
-          </section>
-        </grid-item>
-      </grid-layout>
-    </div>
+            <section>
+              <div v-for="(chartInfo,chartIndex) in item._activeCharts" :key="chartIndex">
+                <CustomChart v-if="['line','bar'].includes(chartInfo.chartType)" :chartInfo="chartInfo" :chartIndex="index" :params="viewCondition"></CustomChart>
+                <CustomPieChart v-if="chartInfo.chartType === 'pie'" :chartInfo="chartInfo" :chartIndex="index" :params="viewCondition"></CustomPieChart>
+              </div>
+            </section>
+          </grid-item>
+        </grid-layout>
+      </div>
+    </section>
   </div>
-
 </template>
 
 <script>
@@ -74,6 +80,7 @@ export default {
   name: '',
   data() {
     return {
+      isPlugin: false,
       viewCondition: {
         timeTnterval: -1800,
         dateRange: ['', ''],
@@ -108,7 +115,11 @@ export default {
     getDashboardData () {
       this.$root.$httpRequestEntrance.httpRequestEntrance('GET',this.$root.apiCenter.template.get, '', responseData => {
         if (responseData.cfg === '' || responseData.cfg === '[]') {
-          this.$router.push({path: 'portal'}) 
+          if (window.request) {
+            this.isPlugin = true
+          } else {
+            this.$router.push({path: 'portal'})
+          }
         }else {
           this.viewData = JSON.parse(responseData.cfg) 
           this.initPanals()
