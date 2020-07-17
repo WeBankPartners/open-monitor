3a3583a81f50de54f2f887c707d29014c0bca1d9
zgyzgyhero <894526647@qq.com>
2020-07-17 18:52:01 +0800

    Upload archive mysql tool

---
+++ monitor-agent/archive_mysql_tool/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"github.com/WeBankPartners/open-monitor/monitor-agent/archive_mysql_tool/funcs"
+)
+
+func main() {
+	cfgFile := flag.String("c", "default.json", "config file")
+	flag.Parse()
+	err := funcs.InitConfig(*cfgFile)
+	if err != nil {
+		log.Printf("init config fail : %v \n", err)
+		return
+	}
+	err = funcs.InitDbEngine()
+	if err != nil {
+		log.Printf("init mysql connect fail : %v \n", err)
+		return
+	}
+	err = funcs.InitMonitorDbEngine()
+	if err != nil {
+		log.Printf("init monitor mysql connect fail : %v \n", err)
+		return
+	}
+	err = funcs.InitMonitorMetricMap()
+	if err != nil {
+		log.Printf("init monitor metric data fail : %v \n", err)
+		return
+	}
+	err = funcs.InitHttpTransport()
+	if err != nil {
+		log.Printf("init prometheus http transport pool fail : %v \n", err)
+		return
+	}
+	funcs.StartCronJob()
+}
