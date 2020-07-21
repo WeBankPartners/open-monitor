62d003e7f895322dfbd5c13b582d6158f829df2b
zgyzgyhero <894526647@qq.com>
2020-07-21 19:42:48 +0800

    Update cron job check alive

--- monitor-server/services/db/process.go
+++ monitor-server/services/db/process.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	m "github.com/WeBankPartners/open-monitor/monitor-server/models"
+	"fmt"
 )
 
 func GetProcessList(endpointId int) (err error, processList []*m.ProcessMonitorTable) {
@@ -22,4 +23,22 @@ func UpdateProcess(param m.ProcessUpdateDto) error {
 		actions = append(actions, &action)
 	}
 	return Transaction(actions)
+}
+
+func UpdateAliveCheckQueue(monitorIp string) error {
+	_,err := x.Exec(fmt.Sprintf("INSERT INTO alive_check_queue(message) VALUE ('%s')", monitorIp))
+	return err
+}
+
+func GetAliveCheckQueue() (err error,result []*m.AliveCheckQueueTable) {
+	err = x.SQL("SELECT * FROM alive_check_queue LIMIT 1").Find(&result)
+	if err != nil {
+		return err,result
+	}
+	if len(result) > 0 {
+		_,err = x.Exec(fmt.Sprintf("DELETE FROM alive_check_queue WHERE message='%s'", result[0].Message))
+	}else{
+		err = fmt.Errorf("alive check queue table is empty")
+	}
+	return err,result
 }
