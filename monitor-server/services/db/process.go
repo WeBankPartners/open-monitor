2b55c5d54af740747fbb1b4fc8ef9939c7bdbf77
zgyzgyhero <894526647@qq.com>
2020-07-22 15:16:40 +0800

    Update cron alive check

--- monitor-server/services/db/process.go
+++ monitor-server/services/db/process.go
@@ -3,6 +3,7 @@ package db
 import (
 	m "github.com/WeBankPartners/open-monitor/monitor-server/models"
 	"fmt"
+	"time"
 )
 
 func GetProcessList(endpointId int) (err error, processList []*m.ProcessMonitorTable) {
@@ -30,15 +31,14 @@ func UpdateAliveCheckQueue(monitorIp string) error {
 	return err
 }
 
-func GetAliveCheckQueue() (err error,result []*m.AliveCheckQueueTable) {
-	err = x.SQL("SELECT * FROM alive_check_queue LIMIT 1").Find(&result)
+func GetAliveCheckQueue(param string) (err error,result []*m.AliveCheckQueueTable) {
+	lastMinDateString := time.Unix(time.Now().Unix()-60, 0).Format("2006-01-02 15:04:05")
+	err = x.SQL(fmt.Sprintf("SELECT * FROM alive_check_queue WHERE message='%s' AND update_at>'%s' LIMIT 1", param, lastMinDateString)).Find(&result)
 	if err != nil {
 		return err,result
 	}
-	if len(result) > 0 {
-		_,err = x.Exec(fmt.Sprintf("DELETE FROM alive_check_queue WHERE message='%s'", result[0].Message))
-	}else{
-		err = fmt.Errorf("alive check queue table is empty")
+	if len(result) == 0 {
+		err = fmt.Errorf("get alive_check_queue table fail,nodata with message=%s and update_at>%s", param, lastMinDateString)
 	}
 	return err,result
 }
