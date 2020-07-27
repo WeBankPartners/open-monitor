efeaad5212b5cb60117d51ab991832cd9f4fad34
zgyzgyhero <894526647@qq.com>
2020-07-27 16:43:40 +0800

    Add archive plugin config

--- monitor-agent/archive_mysql_tool/funcs/http.go
+++ monitor-agent/archive_mysql_tool/funcs/http.go
@@ -12,7 +12,8 @@ func InitHttpHandles()  {
 	if !Config().Http.Enable || Config().Http.Port<=0 {
 		return
 	}
-	http.Handle("/archive/v1/job", http.HandlerFunc(handleCustomJob))
+	http.Handle("/archive/v1/1m/job", http.HandlerFunc(handleCustomJob))
+	http.Handle("/archive/v1/5m/job", http.HandlerFunc(handleFiveMinJob))
 	listenPort := fmt.Sprintf(":%d", Config().Http.Port)
 	log.Printf("listening %s ...\n", listenPort)
 	http.ListenAndServe(listenPort, nil)
@@ -25,7 +26,18 @@ func handleCustomJob(w http.ResponseWriter,r *http.Request)  {
 		returnJson(r,w,err,nil)
 	}else{
 		CreateJob(dateString)
-		returnJson(r,w,err,"start job success")
+		returnJson(r,w,err,"start 1min job success")
+	}
+}
+
+func handleFiveMinJob(w http.ResponseWriter,r *http.Request)  {
+	dateString := r.FormValue("date")
+	t, err := time.Parse("2006-01-02 15:04:05 MST", fmt.Sprintf("%s 00:00:00 CST", dateString))
+	if err != nil {
+		returnJson(r,w,err,nil)
+	}else{
+		ArchiveFromMysql(t.Unix())
+		returnJson(r,w,err,"start 5min job success")
 	}
 }
 
