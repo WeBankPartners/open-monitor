33a5dce1117253502edefa7da858bed011c4d78d
zgyzgyhero <894526647@qq.com>
2020-08-07 16:20:57 +0800

    Update docker build

--- build/build-ui.sh
+++ build/build-ui.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e -x
-cd $(dirname $0)/../monitor-ui
-npm install
-npm run build
+npm -v
+if [ $? -eq 0 ]
+then
+    cd $1/monitor-ui
+    npm install
+    npm run build
+    npm run plugin
+else
+    docker run --rm -v $1:/app/open-monitor --name node-build node:12.13.1 /bin/bash /app/open-monitor/build/build-ui-docker.sh
+fi
