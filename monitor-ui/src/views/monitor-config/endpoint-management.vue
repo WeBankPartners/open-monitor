cbaa6cba588f1b8b2af2a640d3be3c360072c1f3
pobu168 <244202969@qq.com>
2020-09-02 09:37:03 +0800

    #637 Process management

--- monitor-ui/src/views/monitor-config/endpoint-management.vue
+++ monitor-ui/src/views/monitor-config/endpoint-management.vue
@@ -594,6 +594,7 @@
         })
       },
       processManagement (rowData) {
+        this.processConfigModel.processName = ''
         this.id = rowData.id
         this.processConfigModel.addRow.processSet = []
         this.$root.$httpRequestEntrance.httpRequestEntrance('GET','alarm/process/list', {id:this.id}, responseData=> {
@@ -604,14 +605,17 @@
         })
       },
       processConfigSave () {
+        if (this.processConfigModel.processName.trim()) {
+          this.processConfigModel.addRow.processSet.push(this.processConfigModel.processName.trim())
+        }
         const params = {
           endpoint_id: +this.id,
           process_list: this.processConfigModel.addRow.processSet
         }
         this.$root.$httpRequestEntrance.httpRequestEntrance('POST','alarm/process/update', params, ()=> {
           this.$Message.success(this.$t('tips.success'))
-          this.$root.JQ('#process_config_model').modal('hide')
         })
+        this.$root.JQ('#process_config_model').modal('hide')
       },
       addProcess () {
         if (!this.$root.$validate.isEmpty_reset(this.processConfigModel.processName.trim())) {
