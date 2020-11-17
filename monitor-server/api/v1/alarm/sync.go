2346eba9d14f8ce7409d95be15663d672104bbf3
zgyzgyhero <894526647@qq.com>
2020-11-17 17:41:36 +0800

    Update sd config init

--- monitor-server/api/v1/alarm/sync.go
+++ monitor-server/api/v1/alarm/sync.go
@@ -26,6 +26,11 @@ func SyncInitSdFile()  {
 	var stepList []int
 	endpointTable := db.ListEndpoint()
 	for _,v := range endpointTable {
+		if v.ExportType == "ping" || v.ExportType == "telnet" || v.ExportType == "http" {
+			if v.AddressAgent == "" {
+				continue
+			}
+		}
 		tmpAddress := v.Address
 		if v.AddressAgent != "" {
 			tmpAddress = v.AddressAgent
