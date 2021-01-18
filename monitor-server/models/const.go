5526194630fe002a68ecffb041739b2f6df9c1f0
zgyzgyhero <894526647@qq.com>
2021-01-18 16:15:44 +0800

    Update plugin interface auth

--- monitor-server/models/const.go
+++ monitor-server/models/const.go
@@ -6,6 +6,8 @@ const (
 	DateFormatWithZone = `2006-01-02 15:04:05 MST`
 	Version = `1.0.0`
 	TmpCoreToken = `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJTWVNfTU9OSVRPUiIsImlhdCI6MTU5MDExNzM5NSwidHlwZSI6ImFjY2Vzc1Rva2VuIiwiY2xpZW50VHlwZSI6IlNVQl9TWVNURU0iLCJleHAiOjE3NDU2MzczOTUsImF1dGhvcml0eSI6IltTVUJfU1lTVEVNXSJ9.soBixGZNfZKJTsm-augwGsu-fOoPuTYvZNc_VlxS8oAcZsRn4-ccSjEeAvVAso-7y0dxvzz5c_gw9iUE9LVK2w`
+	SystemRole = `SUB_SYSTEM`
+	PlatformUser = `SYS_PLATFORM`
 )
 
 var (
