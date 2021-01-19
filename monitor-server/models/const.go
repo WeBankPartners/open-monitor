8e7e5860e754b99f00fa84eac735425fd0a8c31a
zgyzgyhero <894526647@qq.com>
2021-01-19 17:37:26 +0800

    Update core token auth

--- monitor-server/models/const.go
+++ monitor-server/models/const.go
@@ -5,7 +5,6 @@ const (
 	DatetimeFormat = `2006-01-02 15:04:05`
 	DateFormatWithZone = `2006-01-02 15:04:05 MST`
 	Version = `1.0.0`
-	TmpCoreToken = `***REMOVED***`
 	SystemRole = `SUB_SYSTEM`
 	PlatformUser = `SYS_PLATFORM`
 )
