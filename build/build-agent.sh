9a551fe8308e0fb90d342885805d0a28bedd7946
zgyzgyhero <894526647@qq.com>
2021-08-06 16:18:31 +0800

    update build

--- build/build-agent.sh
+++ build/build-agent.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -x
-mv monitor-agent/node_exporter/node_exporter_new build/conf/node_exporter/
-/bin/cp -f monitor-agent/node_exporter/VERSION build/conf/node_exporter/
-cd build/conf
+mv monitor-agent/node_exporter/node_exporter_new build/node_exporter/
+/bin/cp -f monitor-agent/node_exporter/VERSION build/node_exporter/
+cd build
 tar zcf node_exporter.tar.gz node_exporter
-mv node_exporter.tar.gz ../../
+mv node_exporter.tar.gz ../
