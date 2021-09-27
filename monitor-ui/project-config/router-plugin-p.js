597b07f968d452dd77c80f67e8ca8b66195beb59
pobu168 <244202969@qq.com>
2021-09-27 14:54:01 +0800

    management project files

---
+++ monitor-ui/project-config/router-plugin-p.js
@@ -0,0 +1,36 @@
+const routerP = [
+  { path: "/index",
+   name: "index", 
+   },
+  {
+    path: "/dashboard",
+    name: "dashboard"
+
+  },
+  {
+    path: "/viewConfig",
+    name: "viewConfig"
+  },
+  {
+    path: "/editPieView",
+    name: "editPieView"
+  },
+  {
+    path: "/editLineView",
+    name: "editLineView"
+  },
+  {
+    path: "/viewChart",
+    name: "viewChart"
+  },
+  {
+    path: "/portal",
+    name: "portal"
+  },
+  {
+    path: "/alarmHistory",
+    name: "alarmHistory"
+  }
+];
+
+export default routerP;
