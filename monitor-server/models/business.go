769a0ffaf8cbc8338dd974104d91137bbb8743fe
zgyzgyhero <894526647@qq.com>
2021-09-27 17:00:40 +0800

    Update cron job list

--- monitor-server/models/business.go
+++ monitor-server/models/business.go
@@ -54,3 +54,60 @@ type BusinessAgentDto struct {
 	Path  string  `json:"path"`
 	Config  []*BusinessMonitorCfgObj  `json:"config"`
 }
+
+type PluginBusinessRequest struct {
+	RequestId string                             `json:"requestId"`
+	Inputs    []*PluginBusinessValueRequestObj `json:"inputs"`
+}
+
+type PluginBusinessValueRequestObj struct {
+	CallbackParameter string `json:"callbackParameter"`
+	HostIp            string `json:"hostIp"`
+	RefMonitorObj     string `json:"refMonitorObj"`
+	PathPrefix        string `json:"pathPrefix"`
+	Config            []*PluginBusinessConfigObj `json:"config"`
+}
+
+type PluginBusinessResp struct {
+	ResultCode    string                      `json:"resultCode"`
+	ResultMessage string                      `json:"resultMessage"`
+	Results       PluginBusinessOutput `json:"results"`
+}
+
+type PluginBusinessOutput struct {
+	Outputs []*PluginBusinessOutputObj `json:"outputs"`
+}
+
+type PluginBusinessOutputObj struct {
+	CallbackParameter string `json:"callbackParameter"`
+	HostIp            string `json:"hostIp"`
+	LogPath           string `json:"logPath"`
+	ErrorCode         string `json:"errorCode"`
+	ErrorMessage      string `json:"errorMessage"`
+	ErrorDetail       string `json:"errorDetail,omitempty"`
+}
+
+type PluginBusinessConfigObj struct {
+	Path string `json:"path"`
+	Rules []*PluginBusinessRuleObj `json:"rules"`
+}
+
+type PluginBusinessRuleObj struct {
+	Regular string `json:"regular"`
+	Tags string `json:"tags"`
+	MetricConfig []*PluginBusinessMetricObj `json:"metricConfig"`
+}
+
+type PluginBusinessMetricObj struct {
+	Key  string `json:"key"`
+	Metric string `json:"metric"`
+	Title string `json:"title"`
+	AggType string `json:"aggType"`
+	ValueMap []*PluginBusinessValueMapObj `json:"valueMap"`
+}
+
+type PluginBusinessValueMapObj struct {
+	IsRegular string `json:"isRegular"`
+	StringValue string `json:"stringValue"`
+	IntValue float64 `json:"intValue"`
+}
