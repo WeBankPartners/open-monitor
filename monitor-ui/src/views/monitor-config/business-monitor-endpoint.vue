cae82daa48a125fb910d7029d587172457fd4cab
pobu168 <244202969@qq.com>
2021-12-01 12:43:34 +0800

    update business monitor

---
+++ monitor-ui/src/views/monitor-config/business-monitor-endpoint.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class=" ">
+    endpointasdf
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+
+    }
+  },
+  methods: {},
+  components: {},
+}
+</script>
+
+<style scoped lang="scss">
+</style>
