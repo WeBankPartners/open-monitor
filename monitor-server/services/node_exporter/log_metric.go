961bc0b231134093bf28e4a63af5e87d5e0b6c28
zgyzgyhero <894526647@qq.com>
2021-12-02 11:47:33 +0800

    Update log monitor

--- monitor-server/services/node_exporter/log_metric.go
+++ monitor-server/services/node_exporter/log_metric.go
@@ -1,6 +1,36 @@
 package node_exporter
 
-func SyncLogMetricConfig(serviceGroup string) error {
+import (
+	"fmt"
+	"github.com/WeBankPartners/open-monitor/monitor-server/middleware/log"
+	"github.com/WeBankPartners/open-monitor/monitor-server/models"
+	"github.com/WeBankPartners/open-monitor/monitor-server/services/db"
+)
 
+func UpdateNodeExportConfig(endpoints []string) error {
+	var err error
+	existMap := make(map[string]int)
+	for _,v := range endpoints {
+		if _,b:=existMap[v];b {
+			continue
+		}
+		err = updateEndpointLogMetric(v)
+		if err != nil {
+			err = fmt.Errorf("Sync endpoint:%s log metric config fail,%s ", v, err.Error())
+			break
+		}
+		existMap[v] = 1
+	}
+	return err
+}
+
+func updateEndpointLogMetric(endpointGuid string) error {
+	logMetricConfig,err := db.GetLogMetricByEndpoint(endpointGuid)
+	if err != nil {
+		return fmt.Errorf("Query endpoint:%s log metric config fail,%s ", endpointGuid, err.Error())
+	}
+	endpointObj := models.EndpointTable{Guid: endpointGuid}
+	db.GetEndpoint(&endpointObj)
+	log.Logger.Debug("sync log metric config", log.String("endpoint", endpointGuid), log.JsonObj("config", logMetricConfig))
 	return nil
 }
