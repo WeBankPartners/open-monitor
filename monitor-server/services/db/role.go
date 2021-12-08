973999dd43c54c8c431114f07b2309e500452d80
zgyzgyhero <894526647@qq.com>
2021-12-08 15:37:10 +0800

    Update alarm

--- monitor-server/services/db/role.go
+++ monitor-server/services/db/role.go
@@ -5,23 +5,25 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/WeBankPartners/open-monitor/monitor-server/middleware/log"
-	m "github.com/WeBankPartners/open-monitor/monitor-server/models"
+	"github.com/WeBankPartners/open-monitor/monitor-server/models"
 	"golang.org/x/net/context/ctxhttp"
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func SyncCoreRoleList()  {
-	if m.CoreUrl == "" {
+	if models.CoreUrl == "" {
 		return
 	}
-	request,err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/platform/v1/roles/retrieve", m.CoreUrl), strings.NewReader(""))
+	log.Logger.Debug("Start sync role list")
+	request,err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/platform/v1/roles/retrieve", models.CoreUrl), strings.NewReader(""))
 	if err != nil {
 		log.Logger.Error("Get core role key new request fail", log.Error(err))
 		return
 	}
-	request.Header.Set("Authorization", m.GetCoreToken())
+	request.Header.Set("Authorization", models.GetCoreToken())
 	res,err := ctxhttp.Do(context.Background(), http.DefaultClient, request)
 	if err != nil {
 		log.Logger.Error("Get core role key ctxhttp request fail", log.Error(err))
@@ -29,10 +31,49 @@ func SyncCoreRoleList()  {
 	}
 	b,_ := ioutil.ReadAll(res.Body)
 	defer res.Body.Close()
-	var result m.CoreRoleDto
+	var result models.CoreRoleDto
 	err = json.Unmarshal(b, &result)
 	if err != nil {
 		log.Logger.Error("Get core role key json unmarshal result", log.Error(err))
 		return
+	}
+	if len(result.Data) == 0 {
+		log.Logger.Warn("Get core role key fail with no data")
+		return
+	}
+	var tableData,insertData,updateData []*models.RoleNewTable
+	x.SQL("SELECT * FROM role_new").Find(&tableData)
+	for _,v := range result.Data {
+		var existFlag,updateFlag bool
+		for _,vv := range tableData {
+			if vv.Guid == v.Name {
+				existFlag = true
+				if vv.DisplayName != v.DisplayName || vv.Email != v.Email {
+					updateFlag = true
+				}
+				break
+			}
+		}
+		if !existFlag {
+			insertData = append(insertData, &models.RoleNewTable{Guid:v.Name, DisplayName:v.DisplayName, Email:v.Email})
+			continue
+		}
+		if updateFlag {
+			updateData = append(updateData, &models.RoleNewTable{Guid:v.Name, DisplayName:v.DisplayName, Email:v.Email})
+		}
+	}
+	nowTime := time.Now().Format(models.DatetimeFormat)
+	var actions []*Action
+	for _,v := range insertData {
+		actions = append(actions, &Action{Sql:"insert into role_new(guid,display_name,email,update_time) value (?,?,?,?)",Param: []interface{}{v.Guid,v.DisplayName,v.Email,nowTime}})
+	}
+	for _,v := range updateData {
+		actions = append(actions, &Action{Sql:"update role_new set display_name=?,email=?,update_time=? where guid=?",Param: []interface{}{v.DisplayName,v.Email,nowTime,v.Guid}})
+	}
+	if len(actions) > 0 {
+		err = Transaction(actions)
+		if err != nil {
+			log.Logger.Error("Sync core role fail", log.Error(err))
+		}
 	}
 }
