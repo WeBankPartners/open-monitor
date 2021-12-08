672916f316312581028179f106ed623cd322d393
zgyzgyhero <894526647@qq.com>
2021-12-08 11:12:21 +0800

    Update node exporter

--- build/node_exporter/start.sh
+++ build/node_exporter/start.sh
@@ -7,11 +7,6 @@ package_path=${tmppath/\/start\.sh/}
 chmod +x $package_path/$bin_name $package_path/control
 mkdir -p /usr/local/monitor/$exporter_type
 mkdir -p /usr/local/monitor/$exporter_type/data
-rm -f /usr/local/monitor/host/VERSION
-/bin/cp -f $package_path/* /usr/local/monitor/$exporter_type/
-cd /usr/local/monitor/$exporter_type/
-rm -f start.sh
-./control stop
 cleanprocess='yes'
 if [ -f /usr/local/monitor/host/VERSION ]
 then
@@ -20,6 +15,11 @@ then
     cleanprocess='no'
   fi
 fi
+rm -f /usr/local/monitor/host/VERSION
+/bin/cp -f $package_path/* /usr/local/monitor/$exporter_type/
+cd /usr/local/monitor/$exporter_type/
+rm -f start.sh
+./control stop
 if [ "$cleanprocess" == "yes" ]
 then
   rm -f /usr/local/monitor/host/data/process_cache.data
