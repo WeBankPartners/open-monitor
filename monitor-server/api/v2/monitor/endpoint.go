eeb1e15e23a08bf8e97e6af36cd913f9b1860584
zgyzgyhero <894526647@qq.com>
2021-12-12 01:11:15 +0800

    update service group

--- monitor-server/api/v2/monitor/endpoint.go
+++ monitor-server/api/v2/monitor/endpoint.go
@@ -9,19 +9,19 @@ import (
 )
 
 func ListEndpoint(c *gin.Context) {
-	page,_ := strconv.Atoi(c.Query("page"))
-	size,_ := strconv.Atoi(c.Query("size"))
+	page, _ := strconv.Atoi(c.Query("page"))
+	size, _ := strconv.Atoi(c.Query("size"))
 	monitorType := c.Query("monitorType")
 	param := models.QueryRequestParam{}
 	if size > 0 {
 		param.Paging = true
-		param.Pageable = &models.PageInfo{PageSize: size,StartIndex: page-1}
+		param.Pageable = &models.PageInfo{PageSize: size, StartIndex: page - 1}
 	}
 	if monitorType != "" {
-		param.Filters = []*models.QueryRequestFilterObj{{Name: "monitor_type",Operator: "eq",Value: monitorType}}
+		param.Filters = []*models.QueryRequestFilterObj{{Name: "monitor_type", Operator: "eq", Value: monitorType}}
 	}
 	pageInfo, rowData, err := db.ListEndpoint(&param)
-	returnData := models.TableData{Data: rowData,Page: page,Size: size,Num: pageInfo.TotalRows}
+	returnData := models.TableData{Data: rowData, Page: page, Size: size, Num: pageInfo.TotalRows}
 	if err != nil {
 		middleware.ReturnHandleError(c, err.Error(), err)
 	} else {
@@ -29,22 +29,23 @@ func ListEndpoint(c *gin.Context) {
 	}
 }
 
-func ListMetric(c *gin.Context)  {
+func ListMetric(c *gin.Context) {
 	guid := c.Query("guid")
 	monitorType := c.Query("monitorType")
-	result,err := db.MetricListNew(guid, monitorType)
+	serviceGroup := c.Query("serviceGroup")
+	result, err := db.MetricListNew(guid, monitorType, serviceGroup)
 	if err != nil {
 		middleware.ReturnHandleError(c, err.Error(), err)
-	}else{
+	} else {
 		middleware.ReturnSuccessData(c, result)
 	}
 }
 
-func GetSysMetricTemplate(c *gin.Context)  {
-	result,err := db.GetSysMetricTemplateConfig()
+func GetSysMetricTemplate(c *gin.Context) {
+	result, err := db.GetSysMetricTemplateConfig()
 	if err != nil {
 		middleware.ReturnHandleError(c, err.Error(), err)
-	}else{
+	} else {
 		middleware.ReturnSuccessData(c, result)
 	}
 }
