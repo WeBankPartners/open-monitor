657f905954a7ea366843a4c6c5fe159d1ccc6fd0
zgyzgyhero <894526647@qq.com>
2021-12-13 01:30:29 +0800

    update dashboard

--- monitor-server/models/service_group.go
+++ monitor-server/models/service_group.go
@@ -10,26 +10,34 @@ type ServiceGroupTable struct {
 }
 
 type EndpointServiceRelTable struct {
-	Guid string `json:"guid" xorm:"guid"`
-	Endpoint string `json:"endpoint" xorm:"endpoint"`
+	Guid         string `json:"guid" xorm:"guid"`
+	Endpoint     string `json:"endpoint" xorm:"endpoint"`
 	ServiceGroup string `json:"service_group" xorm:"service_group"`
 }
 
 type ServiceGroupLinkNode struct {
-	Guid string `json:"guid"`
-	Parent *ServiceGroupLinkNode `json:"parent"`
+	Guid     string                  `json:"guid"`
+	Parent   *ServiceGroupLinkNode   `json:"parent"`
 	Children []*ServiceGroupLinkNode `json:"children"`
 }
 
 func (s *ServiceGroupLinkNode) FetchChildGuid() []string {
 	result := []string{s.Guid}
-	for _,v := range s.Children {
+	for _, v := range s.Children {
 		result = append(result, v.FetchChildGuid()...)
 	}
 	return result
 }
 
+func (s *ServiceGroupLinkNode) FetchParentGuid() []string {
+	result := []string{s.Guid}
+	if s.Parent != nil {
+		result = append(result, s.Parent.FetchParentGuid()...)
+	}
+	return result
+}
+
 type ServiceGroupEndpointListObj struct {
-	Guid string `json:"guid"`
+	Guid        string `json:"guid"`
 	DisplayName string `json:"display_name"`
 }
