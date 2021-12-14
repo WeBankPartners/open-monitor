fc84ef0083f87dee511586e675934d96a32350c4
zgyzgyhero <894526647@qq.com>
2021-12-14 09:42:02 +0800

    Update metric config query

--- monitor-server/models/endpoint_group.go
+++ monitor-server/models/endpoint_group.go
@@ -134,6 +134,7 @@ type GroupStrategyObj struct {
 
 type EndpointStrategyObj struct {
 	EndpointGroup string              `json:"endpoint_group"`
+	DisplayName string          `json:"display_name"`
 	MonitorType string `json:"monitor_type"`
 	ServiceGroup  string              `json:"service_group"`
 	Strategy      []*GroupStrategyObj `json:"strategy"`
