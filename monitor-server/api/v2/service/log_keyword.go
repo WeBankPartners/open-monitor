b3695ef00680af3870c14402f8f8ae752de8a720
zgyzgyhero <894526647@qq.com>
2021-12-15 10:08:37 +0800

    Update endpoint change

---
+++ monitor-server/api/v2/service/log_keyword.go
@@ -0,0 +1,19 @@
+package service
+
+import "github.com/gin-gonic/gin"
+
+func ListLogKeywordMonitor(c *gin.Context)  {
+	
+}
+
+func AddLogKeywordMonitor(c *gin.Context)  {
+
+}
+
+func UpdateLogKeywordMonitor(c *gin.Context)  {
+
+}
+
+func DeleteLogKeywordMonitor(c *gin.Context)  {
+
+}
