3ccba89e3c80e685ca32a01c5e116ddd0b75b8ee
zgyzgyhero <894526647@qq.com>
2021-12-16 23:46:26 +0800

    Update log keyword

--- monitor-server/api/v1/alarm/organization.go
+++ monitor-server/api/v1/alarm/organization.go
@@ -1,14 +1,14 @@
 package alarm
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/WeBankPartners/open-monitor/monitor-server/services/db"
+	"fmt"
 	mid "github.com/WeBankPartners/open-monitor/monitor-server/middleware"
 	m "github.com/WeBankPartners/open-monitor/monitor-server/models"
-	"fmt"
+	"github.com/WeBankPartners/open-monitor/monitor-server/services/db"
+	"github.com/gin-gonic/gin"
 )
 
-func GetOrganizaionList(c *gin.Context)  {
+func GetOrganizaionList(c *gin.Context) {
 	nameText := c.Query("name")
 	endpointText := c.Query("endpoint")
 	data, err := db.GetOrganizationList(nameText, endpointText)
@@ -19,28 +19,46 @@ func GetOrganizaionList(c *gin.Context)  {
 	mid.ReturnSuccessData(c, data)
 }
 
-func UpdateOrgPanel(c *gin.Context)  {
+func UpdateOrgPanel(c *gin.Context) {
 	var param m.UpdateOrgPanelParam
 	operation := c.Param("name")
-	if err := c.ShouldBindJSON(&param); err==nil {
-		err = db.UpdateOrganization(operation, param)
-		if err != nil {
-			mid.ReturnUpdateTableError(c, "panel_recursive", err)
-		}else{
-			mid.ReturnSuccess(c)
+	if err := c.ShouldBindJSON(&param); err == nil {
+		if operation == "delete" && param.Force != "yes" {
+			result, getErr := db.GetDeleteServiceGroupAffectList(param.Guid)
+			if getErr != nil {
+				mid.ReturnHandleError(c, getErr.Error(), getErr)
+			} else {
+				if len(result) == 0 {
+					err = db.UpdateOrganization(operation, param)
+					if err != nil {
+						mid.ReturnUpdateTableError(c, "panel_recursive", err)
+					} else {
+						mid.ReturnSuccess(c)
+					}
+				} else {
+					mid.ReturnSuccessData(c, result)
+				}
+			}
+		} else {
+			err = db.UpdateOrganization(operation, param)
+			if err != nil {
+				mid.ReturnUpdateTableError(c, "panel_recursive", err)
+			} else {
+				mid.ReturnSuccess(c)
+			}
 		}
-	}else{
+	} else {
 		mid.ReturnValidateError(c, err.Error())
 	}
 }
 
-func GetOrgPanelRole(c *gin.Context)  {
+func GetOrgPanelRole(c *gin.Context) {
 	guid := c.Query("guid")
 	if guid == "" {
 		mid.ReturnParamEmptyError(c, "guid")
 		return
 	}
-	data,err := db.GetOrgRole(guid)
+	data, err := db.GetOrgRole(guid)
 	if err != nil {
 		mid.ReturnFetchDataError(c, "panel_recursive", "guid", guid)
 		return
@@ -48,27 +66,27 @@ func GetOrgPanelRole(c *gin.Context)  {
 	mid.ReturnSuccessData(c, data)
 }
 
-func UpdateOrgPanelRole(c *gin.Context)  {
+func UpdateOrgPanelRole(c *gin.Context) {
 	var param m.UpdateOrgPanelRoleParam
-	if err := c.ShouldBindJSON(&param); err==nil {
+	if err := c.ShouldBindJSON(&param); err == nil {
 		err = db.UpdateOrgRole(param)
 		if err != nil {
 			mid.ReturnUpdateTableError(c, "panel_recursive", err)
 			return
 		}
 		mid.ReturnSuccess(c)
-	}else{
+	} else {
 		mid.ReturnValidateError(c, err.Error())
 	}
 }
 
-func GetOrgPanelEndpoint(c *gin.Context)  {
+func GetOrgPanelEndpoint(c *gin.Context) {
 	guid := c.Query("guid")
 	if guid == "" {
 		mid.ReturnParamEmptyError(c, "guid")
 		return
 	}
-	data,err := db.GetOrgEndpoint(guid)
+	data, err := db.GetOrgEndpoint(guid)
 	if err != nil {
 		mid.ReturnHandleError(c, "get organization endpoint fail", err)
 		return
@@ -76,41 +94,41 @@ func GetOrgPanelEndpoint(c *gin.Context)  {
 	mid.ReturnSuccessData(c, data)
 }
 
-func UpdateOrgPanelEndpoint(c *gin.Context)  {
+func UpdateOrgPanelEndpoint(c *gin.Context) {
 	var param m.UpdateOrgPanelEndpointParam
-	if err := c.ShouldBindJSON(&param); err==nil {
+	if err := c.ShouldBindJSON(&param); err == nil {
 		err = db.UpdateOrgEndpoint(param)
 		if err != nil {
 			mid.ReturnUpdateTableError(c, "panel_recursive", err)
 			return
 		}
 		mid.ReturnSuccess(c)
-	}else{
+	} else {
 		mid.ReturnValidateError(c, err.Error())
 	}
 }
 
-func UpdateOrgConnect(c *gin.Context)  {
+func UpdateOrgConnect(c *gin.Context) {
 	var param m.UpdateOrgConnectParam
-	if err := c.ShouldBindJSON(&param); err==nil {
+	if err := c.ShouldBindJSON(&param); err == nil {
 		err = db.UpdateOrgConnect(param)
 		if err != nil {
 			mid.ReturnUpdateTableError(c, "panel_recursive", err)
 			return
 		}
 		mid.ReturnSuccess(c)
-	}else{
+	} else {
 		mid.ReturnValidateError(c, err.Error())
 	}
 }
 
-func GetOrgConnect(c *gin.Context)  {
+func GetOrgConnect(c *gin.Context) {
 	guid := c.Query("guid")
 	if guid == "" {
 		mid.ReturnParamEmptyError(c, "guid")
 		return
 	}
-	data,err := db.GetOrgConnect(guid)
+	data, err := db.GetOrgConnect(guid)
 	if err != nil {
 		mid.ReturnFetchDataError(c, "panel_recursive", "guid", guid)
 		return
@@ -118,37 +136,37 @@ func GetOrgConnect(c *gin.Context)  {
 	mid.ReturnSuccessData(c, data)
 }
 
-func IsPluginMode(c *gin.Context)  {
-	result := m.IsPluginModeResult{IsPlugin:true}
+func IsPluginMode(c *gin.Context) {
+	result := m.IsPluginModeResult{IsPlugin: true}
 	if m.CoreUrl == "" {
 		result.IsPlugin = false
 	}
 	mid.ReturnSuccessData(c, result)
 }
 
-func GetOrgPanelEventList(c *gin.Context)  {
+func GetOrgPanelEventList(c *gin.Context) {
 	guid := c.Query("guid")
 	if guid == "" {
 		mid.ReturnParamEmptyError(c, "guid")
 		return
 	}
-	eventList,err := db.GetCoreEventList()
+	eventList, err := db.GetCoreEventList()
 	if err != nil {
 		mid.ReturnHandleError(c, "get core event list fail", err)
 		return
 	}
-	recordData,err := db.GetOrgCallback(guid)
+	recordData, err := db.GetOrgCallback(guid)
 	if err != nil {
 		mid.ReturnFetchDataError(c, "panel_recursive", "guid", guid)
 		return
 	}
 	var result m.GetOrgPanelCallbackData
-	result.FiringCallback = append(result.FiringCallback, &m.OptionModel{OptionText:"default empty",OptionValue:""})
-	result.RecoverCallback = append(result.RecoverCallback, &m.OptionModel{OptionText:"default empty",OptionValue:""})
-	var firingFlag,recoverFlag bool
-	for _,v := range eventList.Data {
-		firingOption := m.OptionModel{OptionText:fmt.Sprintf("%s %s", v.ProcDefName, v.CreatedTime), OptionValue:v.ProcDefKey}
-		recoverOption := m.OptionModel{OptionText:fmt.Sprintf("%s %s", v.ProcDefName, v.CreatedTime), OptionValue:v.ProcDefKey}
+	result.FiringCallback = append(result.FiringCallback, &m.OptionModel{OptionText: "default empty", OptionValue: ""})
+	result.RecoverCallback = append(result.RecoverCallback, &m.OptionModel{OptionText: "default empty", OptionValue: ""})
+	var firingFlag, recoverFlag bool
+	for _, v := range eventList.Data {
+		firingOption := m.OptionModel{OptionText: fmt.Sprintf("%s %s", v.ProcDefName, v.CreatedTime), OptionValue: v.ProcDefKey}
+		recoverOption := m.OptionModel{OptionText: fmt.Sprintf("%s %s", v.ProcDefName, v.CreatedTime), OptionValue: v.ProcDefKey}
 		if v.ProcDefKey == recordData.FiringCallbackKey && firingFlag == false {
 			firingOption.Active = true
 			firingFlag = true
@@ -169,21 +187,21 @@ func GetOrgPanelEventList(c *gin.Context)  {
 	mid.ReturnSuccessData(c, result)
 }
 
-func UpdateOrgPanelCallback(c *gin.Context)  {
+func UpdateOrgPanelCallback(c *gin.Context) {
 	var param m.UpdateOrgPanelEventParam
-	if err := c.ShouldBindJSON(&param); err==nil {
+	if err := c.ShouldBindJSON(&param); err == nil {
 		err = db.UpdateOrgCallback(param)
 		if err != nil {
 			mid.ReturnUpdateTableError(c, "panel_recursive", err)
 			return
 		}
 		mid.ReturnSuccess(c)
-	}else{
+	} else {
 		mid.ReturnValidateError(c, err.Error())
 	}
 }
 
-func SearchSysPanelData(c *gin.Context)  {
+func SearchSysPanelData(c *gin.Context) {
 	search := c.Query("search")
 	endpoint := c.Query("endpoint")
 	if !mid.IsIllegalNormalInput(search) {
