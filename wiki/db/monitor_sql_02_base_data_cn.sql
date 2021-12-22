94e35f6276f2909a02c496a32b105fb5f2583bac
zgyzgyhero <894526647@qq.com>
2021-12-22 18:42:34 +0800

    Update init sql

--- wiki/db/monitor_sql_02_base_data_cn.sql
+++ wiki/db/monitor_sql_02_base_data_cn.sql
@@ -329,7 +329,7 @@ CREATE TABLE `monitor_type` (
   `description` varchar(255)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-insert into monitor_type(guid,display_name) value ('host','host'),('mysql','mysql'),('redis','redis'),('java','java'),('nginx','nginx'),('ping','ping'),('telnet','telnet'),('http','http'),('windows','windows'),('snmp','snmp'),('process','process'),('pod','pod');
+insert into monitor_type(guid,display_name) select t1.* from (select distinct export_type as guid,export_type as display_name from endpoint union select dashboard_type as guid,dashboard_type as display_name from dashboard) t1;
 
 CREATE TABLE `endpoint_new` (
   `guid` varchar(128) NOT NULL PRIMARY KEY,
@@ -524,7 +524,7 @@ CREATE TABLE `notify_role_rel` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 alter table alarm add column alarm_strategy varchar(64);
-insert into role_new(guid,display_name,email) select name,display_name,email from role;
+insert into role_new(guid,display_name,email) select distinct name,display_name,email from role;
 insert into endpoint_group(guid,display_name,description,monitor_type) select name,name,description,endpoint_type from grp where endpoint_type is not null;
 insert into metric(guid,metric,monitor_type,prom_expr,tag_owner) select t1.* from (select CONCAT(metric,'__',metric_type),metric,metric_type,prom_ql,prom_main from prom_metric where metric_type<>'tomcat' union select CONCAT(metric,'__java'),metric,'java',prom_ql,prom_main from prom_metric where metric_type='tomcat') t1;
 insert into alarm_strategy select CONCAT('old_',t1.id),t3.name,t4.guid ,t1.cond,t1.`last`,t1.priority,t1.content,t1.notify_enable,t1.notify_delay,'' as update_time from strategy t1 left join tpl t2 on t1.tpl_id=t2.id left join grp t3 on t2.grp_id=t3.id left join metric t4 on (t1.metric=t4.metric and t3.endpoint_type=t4.monitor_type) where t2.grp_id>0 and t4.guid is not null;
@@ -629,3 +629,12 @@ insert into sys_parameter(guid,param_key,param_value) value ('metric_template_10
 insert into sys_parameter(guid,param_key,param_value) value ('metric_template_11','service_metric_template','{"name":"percent[(sum(a)-sum(b))/sum(a)]","prom_expr":"100*((sum(@a)-sum(@b))/(sum(@a) > 0) or vector(0))","param":"@a,@b"}');
 insert into sys_parameter(guid,param_key,param_value) value ('metric_template_12','service_metric_template','{"name":"percent[sum(a)/(sum(a)+sum(b))]","prom_expr":"100*(sum(@a)/((sum(@a)+sum(@b)) > 0) or vector(0))","param":"@a,@b"}');
 #@v1.13.0.25-end@;
+
+#@v2.0.0-begin@;
+insert into notify(guid,endpoint_group,alarm_action) select concat(guid,'_firing'),guid,'firing' from endpoint_group;
+insert into notify(guid,endpoint_group,alarm_action) select concat(guid,'_ok'),guid,'ok' from endpoint_group;
+insert into endpoint_new(guid,name,ip,monitor_type,agent_address,step,endpoint_address,cluster) select guid,name,ip,export_type,address_agent,step,address,cluster from endpoint where address_agent<>'';
+insert into endpoint_new(guid,name,ip,monitor_type,agent_address,step,endpoint_address,cluster) select guid,name,ip,export_type,address,step,address,cluster from endpoint where address_agent='';
+insert into service_group(guid,display_name,service_type) select guid,display_name,obj_type from panel_recursive where parent='';
+insert into service_group(guid,display_name,service_type,parent) select guid,display_name,obj_type,parent from panel_recursive where parent<>'';
+#@v2.0.0-end@;
