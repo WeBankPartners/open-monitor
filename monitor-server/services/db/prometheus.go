d9fd0899ea9cda2a79e9ae4b5fa6d36430182e8f
zgyzgyhero <894526647@qq.com>
2022-01-10 17:45:01 +0800

    Update prometheus config sync job

--- monitor-server/services/db/prometheus.go
+++ monitor-server/services/db/prometheus.go
@@ -106,7 +106,7 @@ func checkRuleConfigTime(refreshAll bool) {
 	querySql := "select guid from endpoint_group"
 	if !refreshAll {
 		nowTime := time.Unix(sdConfigSyncTime, 0).Format(models.DatetimeFormat)
-		querySql = fmt.Sprintf("select endpoint_group from alarm_strategy where update_time>'%s' union select endpoint_group from endpoint_group_rel where update_time>'%s' union select guid as 'endpoint_group' from endpoint_group where service_group in (select service_group from endpoint_service_rel where update_time>'%s')", nowTime, nowTime, nowTime)
+		querySql = fmt.Sprintf("select endpoint_group as 'guid' from alarm_strategy where update_time>'%s' union select endpoint_group as 'guid' from endpoint_group_rel where update_time>'%s' union select guid from endpoint_group where service_group in (select service_group from endpoint_service_rel where update_time>'%s')", nowTime, nowTime, nowTime)
 	}
 	err := x.SQL(querySql).Find(&endpointGroup)
 	if err != nil {
