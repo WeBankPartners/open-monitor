44a162946bb528c3aca082756e3fa37e3efa2ba4
zgyzgyhero <894526647@qq.com>
2022-09-08 21:32:23 +0800

    Add log metric monitor export import

--- monitor-server/api/v2/service/log_metric.go
+++ monitor-server/api/v2/service/log_metric.go
@@ -1,12 +1,16 @@
 package service
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/WeBankPartners/open-monitor/monitor-server/middleware"
 	"github.com/WeBankPartners/open-monitor/monitor-server/models"
 	"github.com/WeBankPartners/open-monitor/monitor-server/services/db"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
 	"strings"
+	"time"
 )
 
 func ListLogMetricMonitor(c *gin.Context) {
@@ -79,10 +83,10 @@ func UpdateLogMetricMonitor(c *gin.Context) {
 		return
 	}
 	hostEndpointList := []string{}
-	for _,v := range result.EndpointRel {
+	for _, v := range result.EndpointRel {
 		hostEndpointList = append(hostEndpointList, v.SourceEndpoint)
 	}
-	for _,v := range param.EndpointRel {
+	for _, v := range param.EndpointRel {
 		hostEndpointList = append(hostEndpointList, v.SourceEndpoint)
 	}
 	err = db.UpdateLogMetricMonitor(&param)
@@ -278,5 +282,51 @@ func GetServiceGroupEndpointRel(c *gin.Context) {
 		middleware.ReturnHandleError(c, err.Error(), err)
 	} else {
 		middleware.ReturnSuccessData(c, result)
+	}
+}
+
+func ExportLogMetric(c *gin.Context) {
+	serviceGroup := c.Query("serviceGroup")
+	result, err := db.GetLogMetricByServiceGroup(serviceGroup)
+	if err != nil {
+		middleware.ReturnHandleError(c, err.Error(), err)
+		return
+	}
+	b, marshalErr := json.Marshal(result)
+	if marshalErr != nil {
+		middleware.ReturnHandleError(c, "export log metric fail, json marshal object error", marshalErr)
+		return
+	}
+	c.Writer.Header().Add("Content-Disposition", fmt.Sprintf("attachment; filename=%s_%s_%s.json", "log_metric_", result.DisplayName, time.Now().Format("20060102150405")))
+	c.Data(http.StatusOK, "application/octet-stream", b)
+}
+
+func ImportLogMetric(c *gin.Context) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		middleware.ReturnValidateError(c, err.Error())
+		return
+	}
+	f, err := file.Open()
+	if err != nil {
+		middleware.ReturnHandleError(c, "file open error ", err)
+		return
+	}
+	var paramObj models.LogMetricQueryObj
+	b, err := ioutil.ReadAll(f)
+	defer f.Close()
+	if err != nil {
+		middleware.ReturnHandleError(c, "read content fail error ", err)
+		return
+	}
+	err = json.Unmarshal(b, &paramObj)
+	if err != nil {
+		middleware.ReturnHandleError(c, "json unmarshal fail error ", err)
+		return
+	}
+	if err = db.ImportLogMetric(&paramObj); err != nil {
+		middleware.ReturnHandleError(c, "import log metric fail", err)
+	} else {
+		middleware.ReturnSuccess(c)
 	}
 }
