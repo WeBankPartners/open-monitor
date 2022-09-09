67b4ec31d82ec25faddf139240cba43f1acc6e47
zgyzgyhero <894526647@qq.com>
2022-09-09 17:36:36 +0800

    Add log keyword monitor export import

--- monitor-server/api/v2/service/log_keyword.go
+++ monitor-server/api/v2/service/log_keyword.go
@@ -1,10 +1,15 @@
 package service
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/WeBankPartners/open-monitor/monitor-server/middleware"
 	"github.com/WeBankPartners/open-monitor/monitor-server/models"
 	"github.com/WeBankPartners/open-monitor/monitor-server/services/db"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
 func ListLogKeywordMonitor(c *gin.Context) {
@@ -145,4 +150,63 @@ func syncLogKeywordMonitorConfig(logKeywordMonitor string) error {
 func syncLogKeywordNodeExporterConfig(endpointList []string) error {
 	err := db.SyncLogKeywordExporterConfig(endpointList)
 	return err
+}
+
+func ExportLogKeyword(c *gin.Context) {
+	serviceGroup := c.Query("serviceGroup")
+	result, err := db.GetLogKeywordByServiceGroup(serviceGroup)
+	if err != nil {
+		middleware.ReturnHandleError(c, err.Error(), err)
+		return
+	}
+	if len(result) == 0 {
+		middleware.ReturnHandleError(c, "log keyword config list is empty", nil)
+		return
+	}
+	b, marshalErr := json.Marshal(result[0])
+	if marshalErr != nil {
+		middleware.ReturnHandleError(c, "export log metric fail, json marshal object error", marshalErr)
+		return
+	}
+	c.Writer.Header().Add("Content-Disposition", fmt.Sprintf("attachment; filename=%s_%s_%s.json", "log_keyword_", result[0].DisplayName, time.Now().Format("20060102150405")))
+	c.Data(http.StatusOK, "application/octet-stream", b)
+}
+
+func ImportLogKeyword(c *gin.Context) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		middleware.ReturnValidateError(c, err.Error())
+		return
+	}
+	f, err := file.Open()
+	if err != nil {
+		middleware.ReturnHandleError(c, "file open error ", err)
+		return
+	}
+	var paramObj models.LogKeywordServiceGroupObj
+	b, err := ioutil.ReadAll(f)
+	defer f.Close()
+	if err != nil {
+		middleware.ReturnHandleError(c, "read content fail error ", err)
+		return
+	}
+	err = json.Unmarshal(b, &paramObj)
+	if err != nil {
+		middleware.ReturnHandleError(c, "json unmarshal fail error ", err)
+		return
+	}
+	serviceGroup := c.Query("serviceGroup")
+	if serviceGroup == "" {
+		middleware.ReturnValidateError(c, "serviceGroup can not empty")
+		return
+	}
+	paramObj.Guid = serviceGroup
+	for _, logKeyword := range paramObj.Config {
+		logKeyword.ServiceGroup = serviceGroup
+	}
+	if err = db.ImportLogKeyword(&paramObj); err != nil {
+		middleware.ReturnHandleError(c, "import log keyword fail", err)
+	} else {
+		middleware.ReturnSuccess(c)
+	}
 }
