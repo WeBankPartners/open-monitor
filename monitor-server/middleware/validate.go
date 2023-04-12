390daf0e1f912ec8f7ebfd2f9da420fbddc68a5d
zgyzgyhero <894526647@qq.com>
2023-04-12 15:30:57 +0800

    update metric name validate

--- monitor-server/middleware/validate.go
+++ monitor-server/middleware/validate.go
@@ -1,28 +1,29 @@
 package middleware
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"strings"
 	"reflect"
-	"fmt"
 	"regexp"
+	"strings"
 	"sync"
 )
 
 var (
-	invalidData = []string{"select", "insert", "update", "alter", "delete", "drop", "truncate", "show"}
-	regCond = regexp.MustCompile(`^([<=|>=|!=|==|<|>]*)-?\d+(\.\d+)?$`)
-	regLast = regexp.MustCompile(`^\d+[s|m|h]$`)
-	regPath = regexp.MustCompile(`^\/([\w|\.|\-]+\/?)+$`)
-	regNormal = regexp.MustCompile(`^[\w|\.|\-|\~|\!|\@|\#|\$|\%|\^|\[|\]|\{|\}|\(|\)|\,|\s]+$`)
-	regIp = regexp.MustCompile(`^((25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))$`)
-	regActiveWindow = regexp.MustCompile(`^\d{2}:\d{2}-\d{2}:\d{2}$`)
+	invalidData      = []string{"select", "insert", "update", "alter", "delete", "drop", "truncate", "show"}
+	regCond          = regexp.MustCompile(`^([<=|>=|!=|==|<|>]*)-?\d+(\.\d+)?$`)
+	regLast          = regexp.MustCompile(`^\d+[s|m|h]$`)
+	regPath          = regexp.MustCompile(`^\/([\w|\.|\-]+\/?)+$`)
+	regNormal        = regexp.MustCompile(`^[\w|\.|\-|\~|\!|\@|\#|\$|\%|\^|\[|\]|\{|\}|\(|\)|\,|\s]+$`)
+	regIp            = regexp.MustCompile(`^((25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))$`)
+	regActiveWindow  = regexp.MustCompile(`^\d{2}:\d{2}-\d{2}:\d{2}$`)
+	regMetricName    = regexp.MustCompile(`^[\w|\-]+$`)
 	roleEndpointMap  []map[string]int
-	roleEndpointLock  sync.RWMutex
+	roleEndpointLock sync.RWMutex
 )
 
-func ValidateGet(c *gin.Context)  {
+func ValidateGet(c *gin.Context) {
 	isOk := true
 	if c.Request.Method == "GET" {
 		index := strings.Index(c.Request.URL.String(), "?")
@@ -39,7 +40,7 @@ func ValidateGet(c *gin.Context)  {
 	}
 	if isOk {
 		c.Next()
-	}else{
+	} else {
 		ReturnError(c, http.StatusBadRequest, "request get param validate fail", nil)
 		c.Abort()
 		return
@@ -50,9 +51,9 @@ func ValidatePost(c *gin.Context, obj interface{}, ex ...string) bool {
 	isOk := true
 	t := reflect.TypeOf(obj)
 	v := reflect.ValueOf(obj)
-	for k:=0;k < t.NumField();k++ {
+	for k := 0; k < t.NumField(); k++ {
 		isEx := false
-		for _,key := range ex {
+		for _, key := range ex {
 			if key == t.Field(k).Name {
 				isEx = true
 				break
@@ -63,7 +64,7 @@ func ValidatePost(c *gin.Context, obj interface{}, ex ...string) bool {
 		}
 		fieldType := fmt.Sprintf("%v", reflect.TypeOf(v.Field(k).Interface()))
 		if fieldType == "string" || fieldType == "[]string" {
-			for _,inv := range invalidData {
+			for _, inv := range invalidData {
 				if strings.Contains(fmt.Sprintf("%v", v.Field(k).Interface()), inv) {
 					isOk = false
 					break
@@ -86,7 +87,7 @@ func IsIllegalName(str string) bool {
 	if len(str) > 50 {
 		re = true
 	}
-	if strings.TrimSpace(str)=="" {
+	if strings.TrimSpace(str) == "" {
 		re = true
 	}
 	return re
@@ -100,7 +101,7 @@ func IsIllegalLast(str string) bool {
 	return regLast.MatchString(str)
 }
 
-func IsIllegalPath(str string) bool  {
+func IsIllegalPath(str string) bool {
 	return regPath.MatchString(str)
 }
 
@@ -112,18 +113,25 @@ func IsIllegalIp(str string) bool {
 	return !regIp.MatchString(str)
 }
 
+func IsIllegalMetricName(str string) bool {
+	if len(str) > 50 {
+		return true
+	}
+	return !regMetricName.MatchString(str)
+}
+
 func ValidateActiveWindowString(str string) bool {
 	return regActiveWindow.MatchString(str)
 }
 
-func InitRoleEndpointMap()  {
+func InitRoleEndpointMap() {
 
 }
 
-func CheckRoleEndpointOwner(roles []string)  {
+func CheckRoleEndpointOwner(roles []string) {
 
 }
 
-func UpdateRoleEndpointMap()  {
+func UpdateRoleEndpointMap() {
 
 }
