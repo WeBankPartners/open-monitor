1b153c0b8503f980603b9419e74bf7b12c9e03ee
zhijunzhou <2050588049@qq.com>
2023-04-25 13:14:52 +0800

    refactor: circle rorate component

---
+++ monitor-ui/src/components/circle-rotate.vue
@@ -0,0 +1,41 @@
+<template>
+  <img
+    class="circle-img"
+    :width="imgWidth"
+    :height="imgHeight"
+    :src="$attrs.icon"
+    :style="{
+      transform: transformStyle
+    }"
+  />
+</template>
+
+<script>
+export default {
+  name: 'circle-rotate',
+  data() {
+    return {
+      L: 280
+    }
+  },
+  computed: {
+    imgWidth() {
+      const { value, total } = this.$attrs
+      return this.L * ((parseInt(value, 10) / parseInt(total, 10)) || 0)
+    },
+    imgHeight() {
+      return this.imgWidth
+    },
+    transformStyle() {
+      const { deg, tx, ty } = this.$attrs
+      return `rotate(${deg}) translate(${tx * this.imgWidth}px, ${ty * this.imgHeight}px)`
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.circle-img {
+  position: absolute;
+}
+</style>
