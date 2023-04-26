eb659d1e40375caf9666fe56c52ae962ae9cd68a
zhijunzhou <2050588049@qq.com>
2023-04-26 17:58:05 +0800

    refactor: add circle progress animation

--- monitor-ui/src/components/circle-item.vue
+++ monitor-ui/src/components/circle-item.vue
@@ -1,58 +1,84 @@
 <template>
   <div class="m-item">
-    <div class="circle" v-if="$attrs.icon">
-      <div
-        class="inner-circle"
-        :style="circleStyle"
-      >
-        <img class="circle-icon" :src="$attrs.icon" />
-      </div>
-    </div>
+    <vue-ellipse-progress
+      v-if="$attrs.icon"
+      :progress="progressValue"
+      :size="60"
+      :empty-thickness="8"
+      :legend="false"
+      :lineMode="'in -4'"
+      :thickness="4"
+      :color="gradient"
+    >
+      <img
+        slot="legend-caption"
+        class="circle-icon"
+        width="24"
+        height="24"
+        :src="$attrs.icon"
+      />
+    </vue-ellipse-progress>
     <div class="item">
       <div class="title">{{ $attrs.title }}</div>
-      <div class="value" :class="{['text-' + $attrs.type]: true}">{{ $attrs.value }}</div>
+      <div class="value" :class="{ ['text-' + $attrs.type]: true }">
+        {{ $attrs.value }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { VueEllipseProgress } from "vue-ellipse-progress";
+
 const colorMappings = {
   total: {
-    start: '#116ef9',
-    end: '#c1d8fa'
+    start: "#116ef9",
+    end: "#c1d8fa",
   },
   low: {
-    start: '#66cc66',
-    end: '#9ce89c'
+    start: "#66cc66",
+    end: "#9ce89c",
   },
   medium: {
-    start: '#f19d38',
-    end: '#f1c188'
+    start: "#f19d38",
+    end: "#f1c188",
   },
   high: {
-    start: '#da4e2b',
-    end: '#f19881'
-  }
-}
+    start: "#da4e2b",
+    end: "#f19881",
+  },
+};
 export default {
   name: "circle-item",
+  components: { VueEllipseProgress },
   computed: {
-    circleStyle() {
-      const { type, value, total } = this.$attrs
-      const percent = parseInt(value, 10) * 100 / parseInt(total, 10)
-      const percent_1 = percent + 0.2
-
-      if (type === 'total') {
-        return {
-          background: `linear-gradient(135deg, ${colorMappings[type].start} 0%, ${colorMappings[type].end} 100%)`
-        }
+    progressValue() {
+      const { type, value, total } = this.$attrs;
+      if (type === "total") {
+        return 100;
       }
-
+      if (isNaN(total) || total == 0) {
+        return 0;
+      }
+      return (parseInt(value, 10) * 100) / parseInt(total, 10);
+    },
+    gradient() {
+      const { type } = this.$attrs;
       return {
-        background: `conic-gradient(${colorMappings[type].start} 0, ${colorMappings[type].end} ${percent}%, transparent ${percent_1}%, transparent)`
-      }
+        radial: false,
+        colors: [
+          {
+            color: colorMappings[type].start,
+            offset: "0",
+          },
+          {
+            color: colorMappings[type].end,
+            offset: "100",
+          },
+        ],
+      };
     }
-  }
+  },
 };
 </script>
 
@@ -73,59 +99,6 @@ export default {
     background: #f2f3f7;
     border-radius: 50%;
     padding: 3px;
-    .inner-circle {
-      position: relative;
-      height: 53px;
-      width: 53px;
-      flex-shrink: 0;
-      flex-grow: 0;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      border-radius: 50%;
-
-      &.total {
-        background: linear-gradient(135deg, #116ef9 0%, #c1d8fa 100%);
-      }
-
-      &.low {
-        background: conic-gradient(
-          #66cc66 0,
-          #9ce89c 27%,
-          transparent 27.2%,
-          transparent
-        );
-      }
-
-      &.medium {
-        background: conic-gradient(
-          #f19d38 0,
-          #f1c188 27%,
-          transparent 27.2%,
-          transparent
-        );
-      }
-
-      &.high {
-        background: conic-gradient(
-          #da4e2b 0,
-          #f19881 27%,
-          transparent 27.2%,
-          transparent
-        );
-      }
-
-      &::before {
-        content: "";
-        position: absolute;
-        top: 3px;
-        left: 3px;
-        width: 47px;
-        height: 47px;
-        border-radius: 50%;
-        background: #fff;
-      }
-    }
 
     .circle-icon {
       position: absolute;
