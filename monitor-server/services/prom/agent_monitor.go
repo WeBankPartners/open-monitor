30f2dc165c09650d7ddadad1f6d61e8d40849b36
zgyzgyhero <894526647@qq.com>
2023-05-30 16:28:28 +0800

    update agent manager

--- monitor-server/services/prom/agent_monitor.go
+++ monitor-server/services/prom/agent_monitor.go
@@ -14,28 +14,29 @@ import (
 
 var (
 	AgentManagerInitFlag = false
-	AgentManagerLock = new(sync.RWMutex)
+	AgentManagerLock     = new(sync.RWMutex)
 )
 
 type agentManagerRequest struct {
-	Guid  string  `json:"guid"`
-	Exporter  string  `json:"exporter"`
-	Config  string  `json:"config"`
-	InstanceServer  string  `json:"instance_server"`
-	InstancePort  string  `json:"instance_port"`
-	AuthUser  string  `json:"auth_user"`
-	AuthPassword  string  `json:"auth_password"`
+	Guid                 string `json:"guid"`
+	Exporter             string `json:"exporter"`
+	Config               string `json:"config"`
+	InstanceServer       string `json:"instance_server"`
+	InstancePort         string `json:"instance_port"`
+	AuthUser             string `json:"auth_user"`
+	AuthPassword         string `json:"auth_password"`
+	AgentManagerRemoteIp string `json:"agentManagerRemoteIp"`
 }
 
 type agentManagerResponse struct {
-	Code  int  `json:"code"`
-	Message  string  `json:"message"`
-	Data  interface{}  `json:"data"`
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
 
-func DeployAgent(agentType,instance,bin,ip,port,user,pwd,url,configFile string) (address string,err error) {
+func DeployAgent(agentType, instance, bin, ip, port, user, pwd, url, configFile string) (address string, err error) {
 	if !AgentManagerInitFlag {
-		time.Sleep(1*time.Second)
+		time.Sleep(1 * time.Second)
 		AgentManagerLock.RLock()
 		AgentManagerLock.RUnlock()
 	}
@@ -47,52 +48,53 @@ func DeployAgent(agentType,instance,bin,ip,port,user,pwd,url,configFile string) 
 	param.InstancePort = port
 	param.AuthUser = user
 	param.AuthPassword = pwd
-	resp,err := requestAgentMonitor(param,url,"add")
+	param.AgentManagerRemoteIp = m.AgentManagerRemoteIp
+	resp, err := requestAgentMonitor(param, url, "add")
 	if err != nil {
-		return address,err
+		return address, err
 	}
 	if resp.Code == 200 {
 		if strings.Contains(resp.Message, ":") {
 			tmpAddress := resp.Message
-			if strings.Contains(url, "127.0.0.1") {
-				tmpAddress = "127.0.0.1" + tmpAddress[strings.Index(tmpAddress, ":"):]
-			}
-			return tmpAddress,nil
-		}else{
+			tmpAddress = "127.0.0.1" + tmpAddress[strings.Index(tmpAddress, ":"):]
+			return tmpAddress, nil
+		} else {
 			return "", fmt.Errorf("agent manager response message is illegal address: %s ", resp.Message)
 		}
-	}else{
-		return address,fmt.Errorf(resp.Message)
+	} else {
+		return address, fmt.Errorf(resp.Message)
 	}
 }
 
-func StopAgent(agentType,instance,ip,url string) error {
+func StopAgent(agentType, instance, ip, url string) error {
 	var param agentManagerRequest
 	param.Guid = fmt.Sprintf("%s_%s_%s", instance, ip, agentType)
-	resp,err := requestAgentMonitor(param,url,"delete")
+	param.AgentManagerRemoteIp = m.AgentManagerRemoteIp
+	resp, err := requestAgentMonitor(param, url, "delete")
 	if err != nil {
 		return err
 	}
 	if resp.Code == 200 {
 		return nil
-	}else{
+	} else {
 		return fmt.Errorf(resp.Message)
 	}
 }
 
 func InitAgentManager(param []*m.AgentManagerTable, url string) {
 	count := 0
+	initParam := m.InitDeployParam{AgentManagerRemoteIp: m.AgentManagerRemoteIp, Config: param}
 	AgentManagerLock.Lock()
 	for {
-		time.Sleep(30*time.Second)
-		resp, err := requestAgentMonitor(param, url, "init")
+		time.Sleep(30 * time.Second)
+		resp, err := requestAgentMonitor(&initParam, url, "init")
 		if err != nil {
 			log.Logger.Error("Init agent manager, request error", log.Error(err))
 		}
 		if resp.Code == 200 {
 			log.Logger.Info("Init agent manager success")
 			break
-		}else{
+		} else {
 			log.Logger.Warn("Init agent manager, response error", log.String("message", resp.Message))
 		}
 		count++
@@ -105,42 +107,43 @@ func InitAgentManager(param []*m.AgentManagerTable, url string) {
 	AgentManagerInitFlag = true
 }
 
-func DoSyncAgentManagerJob(param []*m.AgentManagerTable, url string)  {
+func DoSyncAgentManagerJob(param []*m.AgentManagerTable, url string) {
 	log.Logger.Info("Start init agent manager ")
-	resp, err := requestAgentMonitor(param, url, "init")
+	initParam := m.InitDeployParam{AgentManagerRemoteIp: m.AgentManagerRemoteIp, Config: param}
+	resp, err := requestAgentMonitor(&initParam, url, "init")
 	if err != nil {
 		log.Logger.Error("Init agent manager, request error", log.Error(err))
 	}
 	if resp.Code == 200 {
 		log.Logger.Info("Init agent manager success")
-	}else{
+	} else {
 		log.Logger.Warn("Init agent manager, response error", log.String("message", resp.Message))
 	}
 }
 
-func requestAgentMonitor(param interface{},url,method string) (resp agentManagerResponse,err error) {
-	postData,err := json.Marshal(param)
+func requestAgentMonitor(param interface{}, url, method string) (resp agentManagerResponse, err error) {
+	postData, err := json.Marshal(param)
 	if err != nil {
 		log.Logger.Error("Failed marshalling data", log.Error(err))
-		return resp,err
+		return resp, err
 	}
-	req,err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/deploy/%s", url, method), strings.NewReader(string(postData)))
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/deploy/%s", url, method), strings.NewReader(string(postData)))
 	if err != nil {
 		log.Logger.Error("Curl agent_monitor http request error", log.Error(err))
-		return resp,err
+		return resp, err
 	}
-	res,err := http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		log.Logger.Error("Curl agent_monitor http response error", log.Error(err))
-		return resp,err
+		return resp, err
 	}
 	defer res.Body.Close()
-	body,_ := ioutil.ReadAll(res.Body)
+	body, _ := ioutil.ReadAll(res.Body)
 	log.Logger.Debug(fmt.Sprintf("Curl %s agent_monitor response : %s ", method, string(body)))
 	err = json.Unmarshal(body, &resp)
 	if err != nil {
 		log.Logger.Error("Curl agent_monitor unmarshal error", log.Error(err))
-		return resp,err
+		return resp, err
 	}
-	return resp,nil
+	return resp, nil
 }
