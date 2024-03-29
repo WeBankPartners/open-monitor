f06829bf8478550a2e9d0bbbc3bc9aff5c3a10b0
wanghao7717 <792974788@qq.com>
2024-03-29 18:27:14 +0800

    获取平台token调整

--- monitor-ui/src/assets/js/cookies.ts
+++ monitor-ui/src/assets/js/cookies.ts
@@ -3,11 +3,12 @@ import Cookies from 'js-cookie'
 // User
 const tokenKey = 'Authorization'
 export const getToken = () => Cookies.get(tokenKey)
-const key = 'accessToken'
+// const key = 'accessToken'
 export const getPlatFormToken = () =>  {
   // eslint-disable-next-line no-useless-escape
-  const reg = new RegExp('(?:(?:^|.*;\\s*)' + key + '\\s*\\=\\s*([^;]*).*$)|^.*$')
-  return document.cookie.replace(reg, '$1')
+  // const reg = new RegExp('(?:(?:^|.*;\\s*)' + key + '\\s*\\=\\s*([^;]*).*$)|^.*$')
+  // return document.cookie.replace(reg, '$1')
+  return localStorage.getItem('wecube-accessToken')
 }
 export const setToken = (token: string) => Cookies.set(tokenKey, token)
 export const removeToken = () => Cookies.remove(tokenKey)
