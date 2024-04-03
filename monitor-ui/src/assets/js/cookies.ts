1430855ef760f0a7b74ff67e88ccc77721ac656e
wanghao7717 <792974788@qq.com>
2024-04-03 16:41:55 +0800

    取token逻辑代码回退

--- monitor-ui/src/assets/js/cookies.ts
+++ monitor-ui/src/assets/js/cookies.ts
@@ -3,12 +3,11 @@ import Cookies from 'js-cookie'
 // User
 const tokenKey = 'Authorization'
 export const getToken = () => Cookies.get(tokenKey)
-// const key = 'accessToken'
+const key = 'accessToken'
 export const getPlatFormToken = () =>  {
   // eslint-disable-next-line no-useless-escape
-  // const reg = new RegExp('(?:(?:^|.*;\\s*)' + key + '\\s*\\=\\s*([^;]*).*$)|^.*$')
-  // return document.cookie.replace(reg, '$1')
-  return localStorage.getItem('wecube-accessToken')
+  const reg = new RegExp('(?:(?:^|.*;\\s*)' + key + '\\s*\\=\\s*([^;]*).*$)|^.*$')
+  return document.cookie.replace(reg, '$1')
 }
 export const setToken = (token: string) => Cookies.set(tokenKey, token)
 export const removeToken = () => Cookies.remove(tokenKey)
