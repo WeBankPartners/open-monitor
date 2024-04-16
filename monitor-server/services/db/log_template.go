629a7041e4da083b84ce40d91eca4e0a27b69fbd
zgyzgyhero <894526647@qq.com>
2024-04-16 20:01:59 +0800

    add log monitor template

---
+++ monitor-server/services/db/log_template.go
@@ -0,0 +1,28 @@
+package db
+
+import "github.com/WeBankPartners/open-monitor/monitor-server/models"
+
+func ListLogMonitorTemplate(userRoles []string) (result []*models.LogMonitorTemplateDto, err error) {
+
+	return
+}
+
+func GetLogMonitorTemplate(guid string) (result []*models.LogMonitorTemplateDto, err error) {
+
+	return
+}
+
+func CreateLogMonitorTemplate(param *models.LogMonitorTemplateDto) (err error) {
+
+	return
+}
+
+func UpdateLogMonitorTemplate(param *models.LogMonitorTemplateDto) (err error) {
+
+	return
+}
+
+func DeleteLogMonitorTemplate(logMonitorTemplateGuid string) (err error) {
+
+	return
+}
