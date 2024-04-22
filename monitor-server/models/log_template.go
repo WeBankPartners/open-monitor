263befc08c26878d3bb1fe5a26650648252bc0fd
zgyzgyhero <894526647@qq.com>
2024-04-22 18:00:00 +0800

    update log monitor template

--- monitor-server/models/log_template.go
+++ monitor-server/models/log_template.go
@@ -3,13 +3,18 @@ package models
 import "time"
 
 type LogMonitorTemplate struct {
-	Guid       string    `json:"guid" xorm:"guid"`
-	Name       string    `json:"name" xorm:"name"`
-	MetricType string    `json:"metric_type" xorm:"metric_type"`
-	CreateUser string    `json:"create_user" xorm:"create_user"`
-	UpdateUser string    `json:"update_user" xorm:"update_user"`
-	CreateTime time.Time `json:"create_time" xorm:"create_time"`
-	UpdateTime time.Time `json:"update_time" xorm:"update_time"`
+	Guid             string    `json:"guid" xorm:"guid"`
+	Name             string    `json:"name" xorm:"name"`
+	LogType          string    `json:"log_type" xorm:"log_type"`
+	DemoLog          string    `json:"demo_log" xorm:"demo_log"`
+	JsonRegular      string    `json:"json_regular" xorm:"json_regular"`
+	CalcResult       string    `json:"-" xorm:"calc_result"`
+	CreateUser       string    `json:"create_user" xorm:"create_user"`
+	UpdateUser       string    `json:"update_user" xorm:"update_user"`
+	CreateTime       time.Time `json:"-" xorm:"create_time"`
+	CreateTimeString string    `json:"create_time"`
+	UpdateTime       time.Time `json:"-" xorm:"update_time"`
+	UpdateTimeString string    `json:"update_time"`
 }
 
 type LogMonitorTemplateRole struct {
@@ -19,13 +24,14 @@ type LogMonitorTemplateRole struct {
 	Permission         string `json:"permission" xorm:"permission"`
 }
 
-type LogJsonTemplate struct {
+type LogParamTemplate struct {
 	Guid               string    `json:"guid" xorm:"guid"`
 	LogMonitorTemplate string    `json:"log_monitor_template" xorm:"log_monitor_template"`
 	Name               string    `json:"name" xorm:"name"`
-	JsonRegular        string    `json:"json_regular" xorm:"json_regular"`
-	DemoLog            string    `json:"demo_log" xorm:"demo_log"`
-	CalcResult         string    `json:"calc_result" xorm:"calc_result"`
+	DisplayName        string    `json:"display_name" xorm:"display_name"`
+	JsonKey            string    `json:"json_key" xorm:"json_key"`
+	Regular            string    `json:"regular" xorm:"regular"`
+	DemoMatchValue     string    `json:"demo_match_value" xorm:"demo_match_value"`
 	CreateUser         string    `json:"create_user" xorm:"create_user"`
 	UpdateUser         string    `json:"update_user" xorm:"update_user"`
 	CreateTime         time.Time `json:"create_time" xorm:"create_time"`
@@ -35,16 +41,13 @@ type LogJsonTemplate struct {
 type LogMetricTemplate struct {
 	Guid               string    `json:"guid" xorm:"guid"`
 	LogMonitorTemplate string    `json:"log_monitor_template" xorm:"log_monitor_template"`
-	LogJsonTemplate    string    `json:"log_json_template" xorm:"log_json_template"`
+	LogParamName       string    `json:"log_param_name" xorm:"log_param_name"`
 	Metric             string    `json:"metric" xorm:"metric"`
 	DisplayName        string    `json:"display_name" xorm:"display_name"`
-	JsonKey            string    `json:"json_key" xorm:"json_key"`
-	Regular            string    `json:"regular" xorm:"regular"`
-	DemoLog            string    `json:"demo_log" xorm:"demo_log"`
-	CalcResult         string    `json:"calc_result" xorm:"calc_result"`
 	Step               int       `json:"step" xorm:"step"`
 	AggType            string    `json:"agg_type" xorm:"agg_type"`
-	TagConfig          string    `json:"tag_config" xorm:"tag_config"`
+	TagConfig          string    `json:"-" xorm:"tag_config"`
+	TagConfigList      []string  `json:"tag_config" xorm:"-"`
 	CreateUser         string    `json:"create_user" xorm:"create_user"`
 	UpdateUser         string    `json:"update_user" xorm:"update_user"`
 	CreateTime         time.Time `json:"create_time" xorm:"create_time"`
@@ -52,15 +55,24 @@ type LogMetricTemplate struct {
 }
 
 type LogMonitorTemplateDto struct {
-	Guid       string                        `json:"guid" xorm:"guid"`
-	Name       string                        `json:"name" xorm:"name"`
-	MetricType string                        `json:"metric_type" xorm:"metric_type"`
-	JsonConfig *LogJsonTemplate              `json:"json_config"`
-	Metrics    []*LogMetricTemplate          `json:"metrics"`
-	Permission *LogMonitorTemplatePermission `json:"permission"`
+	LogMonitorTemplate
+	CalcResultObj *CheckRegExpResult            `json:"calc_result"`
+	ParamList     []*LogParamTemplate           `json:"param_list"`
+	MetricList    []*LogMetricTemplate          `json:"metric_list"`
+	Permission    *LogMonitorTemplatePermission `json:"permission"`
 }
 
 type LogMonitorTemplatePermission struct {
 	MgmtRoles []string `json:"mgmt_roles"`
 	UseRoles  []string `json:"use_roles"`
+}
+
+type LogMonitorTemplateListResp struct {
+	JsonList    []*LogMonitorTemplate `json:"json_list"`
+	RegularList []*LogMonitorTemplate `json:"regular_list"`
+}
+
+type LogMonitorRegMatchParam struct {
+	DemoLog   string              `json:"demo_log"`
+	ParamList []*LogParamTemplate `json:"param_list"`
 }
