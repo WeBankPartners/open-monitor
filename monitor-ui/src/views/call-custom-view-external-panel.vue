3bbea07beee058e4ee903cb8e8487aaed70bd8b7
pobu168 <244202969@qq.com>
2024-05-09 16:27:07 +0800

    add: modules for exporter

--- monitor-ui/src/views/call-custom-view-external-panel.vue
+++ monitor-ui/src/views/call-custom-view-external-panel.vue
@@ -236,7 +236,7 @@ export default {
       viewCondition: {
         timeTnterval: -3600,
         dateRange: ['', ''],
-        autoRefresh: 5,
+        autoRefresh: 10,
         agg: 'none' // 聚合类型
       },
       disableTime: false,
