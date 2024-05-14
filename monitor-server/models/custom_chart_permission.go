b49cbfd25cef9573931aaf8a50a65aa0db92438a
tangjiawei <tangjiawei@nnyy.com>
2024-05-14 15:23:26 +0800

    【自定义视图】自定义看板列表&看板详情接口

---
+++ monitor-server/models/custom_chart_permission.go
@@ -0,0 +1,8 @@
+package models
+
+type CustomChartPermission struct {
+	Guid           string `json:"guid" xorm:"'guid' pk"`
+	DashboardChart string `json:"dashboardChart" xorm:"dashboard_chart"` // 所属看板图表
+	RoleId         string `json:"roleId" xorm:"role_id"`                 // 角色id
+	Permission     string `json:"permission" xorm:"permission"`          // 权限,MGMT/USE
+}
