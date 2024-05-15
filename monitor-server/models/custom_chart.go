379bf14f3895fbc136f72dfdc034e5a806e251b9
tangjiawei <tangjiawei@nnyy.com>
2024-05-15 15:22:31 +0800

    【自定义视图】看板的增删改查,可用图表查询接口开发

--- monitor-server/models/custom_chart.go
+++ monitor-server/models/custom_chart.go
@@ -11,7 +11,7 @@ type CustomChart struct {
 	AggStep         int    `json:"aggStep" xorm:"agg_step"`                 // 聚合间隔
 	Unit            string `json:"unit" xorm:"unit"`                        // 单位
 	CreateUser      string `json:"createUser" xorm:"create_user"`           // 创建人
-	UpdatedUser     string `json:"updatedUser" xorm:"updated_user"`         // 更新人
+	UpdateUser      string `json:"updateUser" xorm:"update_user"`           // 更新人
 	CreateTime      string `json:"createTime" xorm:"create_time"`           // 创建时间
 	UpdateTime      string `json:"updateTime" xorm:"update_time"`           // 更新时间
 }
@@ -28,11 +28,17 @@ type CustomChartDto struct {
 	SourceDashboard string                  `json:"sourceDashboard"` // 源看板
 	Name            string                  `json:"name"`            // 图表名称
 	Unit            string                  `json:"unit"`            // 单位
-	ChartType       string                  `json:"chartType"`       // 曲线图/饼图,line//pie
+	ChartType       string                  `json:"chartType"`       // 曲线图/饼图,line/pie
 	LineType        string                  `json:"lineType"`        // 折线/柱状/面积,line/bar/area
 	Aggregate       string                  `json:"aggregate"`       // 聚合类型
 	AggStep         int                     `json:"aggStep"`         // 聚合间隔
 	Query           []*CustomChartSeriesDto `json:"query"`
 	DisplayConfig   interface{}             `json:"displayConfig"`
 	Group           string                  `json:"group"` // 所属分组
+}
+
+type ChartSharedDto struct {
+	Id              string `json:"id"`
+	SourceDashboard string `json:"sourceDashboard"` // 源看板
+	Name            string `json:"name"`            // 图表名称
 }
