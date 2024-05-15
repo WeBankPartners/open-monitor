e2033d8655d3c7022d9dece106d3df672f5dfb18
Alexaa123 <1239327906@qq.com>
2024-05-15 19:15:21 +0800

    告警部分修改前端开发

--- monitor-ui/src/.eslintrc.js
+++ monitor-ui/src/.eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
     rules: {
     'no-console': 'off',
+    'no-debugger': 'off',
+    'no-unused-vars': 'off',
     'vue/no-parsing-error': [2, { "x-invalid-end-tag": false }]
     }
 }
