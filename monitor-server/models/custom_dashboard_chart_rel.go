06665b6ec83ef7bd1ebe45ab808a4626260a9c8e
tangjiawei <tangjiawei@nnyy.com>
2024-05-15 15:22:31 +0800

    【自定义视图】看板的增删改查,可用图表查询接口开发

--- monitor-server/models/custom_dashboard_chart_rel.go
+++ monitor-server/models/custom_dashboard_chart_rel.go
@@ -7,7 +7,7 @@ type CustomDashboardChartRel struct {
 	Group           string  `json:"group" xorm:"group"`                      // 所属分组
 	DisplayConfig   string  `json:"displayConfig" xorm:"display_config"`     // 视图位置与长宽
 	CreateUser      string  `json:"createUser" xorm:"create_user"`           // 创建人
-	UpdatedUser     string  `json:"updatedUser" xorm:"updated_user"`         // 更新人
+	UpdateUser      string  `json:"updateUser" xorm:"update_user"`           // 更新人
 	CreateTime      string  `json:"createTime" xorm:"create_time"`           // 创建时间
 	UpdateTime      string  `json:"updateTime" xorm:"update_time"`           // 更新时间
 }
