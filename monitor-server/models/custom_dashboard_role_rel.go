1ae9b8643f02a3df272e4f6fbd1b43addfadd345
tangjiawei <tangjiawei@nnyy.com>
2024-05-20 17:39:11 +0800

    【自定义视图】看板新增查询接口自测

--- monitor-server/models/custom_dashboard_role_rel.go
+++ monitor-server/models/custom_dashboard_role_rel.go
@@ -2,7 +2,7 @@ package models
 
 type CustomDashBoardRoleRel struct {
 	Id              int    `json:"id" xorm:"'id' pk"`
-	RoleId          string `json:"roleId" xorm:"role_id"`                   // 角色ID
-	CustomDashboard string `json:"customDashboard" xorm:"custom_dashboard"` // 自定义看板
-	Permission      string `json:"permission" xorm:"permission"`            // 权限
+	RoleId          string `json:"roleId" xorm:"role_id"`                      // 角色ID
+	CustomDashboard string `json:"customDashboard" xorm:"custom_dashboard_id"` // 自定义看板
+	Permission      string `json:"permission" xorm:"permission"`               // 权限
 }
