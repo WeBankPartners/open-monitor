91df20201341e7fb00f9198d353afd6a6f812fc9
tangjiawei <tangjiawei@nnyy.com>
2024-05-21 20:21:35 +0800

    【自定义视图】自定义视图接口增删改查接口自测调整

--- monitor-server/models/custom_chart.go
+++ monitor-server/models/custom_chart.go
@@ -18,9 +18,22 @@ type CustomChart struct {
 }
 
 type CustomChartExtend struct {
-	CustomChart   *CustomChart
-	Group         string `json:"group" xorm:"group"`                  // 所属分组
-	DisplayConfig string `json:"displayConfig" xorm:"display_config"` // 视图位置与长宽
+	Guid            string `json:"id" xorm:"'guid' pk"`
+	SourceDashboard int    `json:"sourceDashboard" xorm:"source_dashboard"` // 源看板
+	Public          int    `json:"public" xorm:"public"`                    // 是否公共
+	Name            string `json:"name" xorm:"name"`                        // 图表名称
+	ChartTemplate   string `json:"chartTemplate" xorm:"chart_template"`     // 图表模板
+	ChartType       string `json:"chartType" xorm:"chart_type"`             // 曲线图/饼图,line//pie
+	LineType        string `json:"lineType" xorm:"line_type"`               // 折线/柱状/面积,line/bar/area
+	Aggregate       string `json:"aggregate" xorm:"aggregate"`              // 聚合类型
+	AggStep         int    `json:"aggStep" xorm:"agg_step"`                 // 聚合间隔
+	Unit            string `json:"unit" xorm:"unit"`                        // 单位
+	CreateUser      string `json:"createUser" xorm:"create_user"`           // 创建人
+	UpdateUser      string `json:"updateUser" xorm:"update_user"`           // 更新人
+	CreateTime      string `json:"createTime" xorm:"create_time"`           // 创建时间
+	UpdateTime      string `json:"updateTime" xorm:"update_time"`           // 更新时间
+	Group           string `json:"group" xorm:"group"`                      // 所属分组
+	DisplayConfig   string `json:"displayConfig" xorm:"display_config"`     // 视图位置与长宽
 }
 
 type CustomChartDto struct {
@@ -99,4 +112,23 @@ type CopyCustomChartParam struct {
 	OriginChartId string      `json:"originChartId"` // 原图表ID
 	Group         string      `json:"group"`         // 所属分组
 	DisplayConfig interface{} `json:"displayConfig"` // 视图位置与长宽
+}
+
+func ConvertCustomChartToExtend(chart *CustomChart) *CustomChartExtend {
+	return &CustomChartExtend{
+		Guid:            chart.Guid,
+		SourceDashboard: chart.SourceDashboard,
+		Public:          chart.Public,
+		Name:            chart.Name,
+		ChartTemplate:   chart.ChartTemplate,
+		ChartType:       chart.ChartType,
+		LineType:        chart.LineType,
+		Aggregate:       chart.Aggregate,
+		AggStep:         chart.AggStep,
+		Unit:            chart.Unit,
+		CreateUser:      chart.CreateUser,
+		UpdateUser:      chart.UpdateUser,
+		CreateTime:      chart.CreateTime,
+		UpdateTime:      chart.UpdateTime,
+	}
 }
