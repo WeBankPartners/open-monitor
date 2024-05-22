58124c78dc87e6b7f7094c6c85e3bba727c27ede
tangjiawei <tangjiawei@nnyy.com>
2024-05-22 09:53:33 +0800

    【自定义视图】自定义视图查询列表权限bug修复

--- monitor-server/services/db/custom_chart.go
+++ monitor-server/services/db/custom_chart.go
@@ -562,13 +562,13 @@ func getChartQueryIdsByPermission(condition models.QueryChartParam, roles []stri
 
 	if len(condition.UseRoles) > 0 {
 		useRoleFilterSql, useRoleFilterParam := createListParams(condition.UseRoles, "")
-		sql = sql + " and role_id  in (" + useRoleFilterSql + ")"
-		params = append(params, useRoleFilterParam...)
+		sql = sql + " and (role_id  in (" + useRoleFilterSql + ") and permission = ?)"
+		params = append(append(params, useRoleFilterParam...), models.PermissionUse)
 	}
 	if len(condition.MgmtRoles) > 0 {
 		mgmtRoleFilterSql, mgmtRoleFilterParam := createListParams(condition.MgmtRoles, "")
-		sql = sql + " and role_id  in (" + mgmtRoleFilterSql + ")"
-		params = append(params, mgmtRoleFilterParam...)
+		sql = sql + " and (role_id  in (" + mgmtRoleFilterSql + ") and permission = ?)"
+		params = append(append(params, mgmtRoleFilterParam...), models.PermissionMgmt)
 	}
 	if condition.Permission == string(models.PermissionMgmt) {
 		sql = sql + " and permission = ? "
