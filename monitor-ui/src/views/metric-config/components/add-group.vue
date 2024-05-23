54b236eece12e8a7b4d498d9d568bac71a581df6
wanghao7717 <792974788@qq.com>
2024-05-23 20:58:19 +0800

    feat: 指标配置页面重构

---
+++ monitor-ui/src/views/metric-config/components/add-group.vue
@@ -0,0 +1,362 @@
+<template>
+  <div>
+    <Drawer
+      v-model="drawerVisible"
+      width="720"
+      :mask-closable="false"
+      :lock-scroll="true"
+      @on-close="handleCancel"
+      class="monitor-add-group"
+    >
+      <div slot="header" class="w-header">
+        <div class="title">{{ operator === 'add' ? '新增指标' : '编辑指标' }}<span class="underline"></span></div>
+        <slot name="sub-title"></slot>
+      </div>
+      <div class="content" :style="{ maxHeight: maxHeight + 'px' }">
+        <Form :label-width="100" label-position="left">
+          <!--名称-->
+          <FormItem :label="$t('tableKey.name')">
+            <Input :disabled="operator === 'edit'" v-model="metricConfigData.metric"></Input>
+          </FormItem>
+          <!--作用域-->
+          <FormItem :label="$t('m_scope')">
+            <Select v-model="workspace" @on-change="changeWorkspace">
+              <Option v-if="serviceGroup" value="all_object">{{ $t('m_all_object') }}</Option>
+              <Option value="any_object">{{ $t('m_any_object') }}</Option>
+            </Select>
+          </FormItem>
+          <!--推荐配置-->
+          <FormItem :label="$t('m_recommend')">
+            <Select v-model="templatePl" clearable @on-clear="clearTemplatePl" @on-change="changeTemplatePl">
+              <Option v-for="item in metricTemplate" :value="item.prom_expr" :key="item.prom_expr">{{ item.name }}</Option>
+            </Select>
+          </FormItem>
+          <!--采集数据-->
+          <FormItem v-if="templatePl" :label="$t('m_collected_data')">
+            {{templatePl}}
+            <template v-for="param in metricTemplateParams">
+              <Select
+                v-model="param.value"
+                @on-open-change="getCollectedMetric"
+                @on-change="changeCollectedMetric(param)"
+                filterable
+                :key="param.label"
+                :placeholder="param.label"
+                class="select-dropdown"
+              >
+                <Option 
+                  style="white-space: normal;"
+                  v-for="(item, itemIndex) in collectedMetricOptions" 
+                  :value="item.option_value" 
+                  :key="item.option_value + itemIndex">
+                  {{ item.option_text }}
+                </Option>
+              </Select>
+            </template>
+          </FormItem>
+          <!--表达式-->
+          <FormItem :label="$t('field.metric')">
+            <Input v-model="metricConfigData.prom_expr" type="textarea" :rows="5" style="margin:5px 0;" />
+          </FormItem>
+          <!--预览对象-->
+          <FormItem :label="$t('m_preview') + $t('m_endpoint')">
+            <Select filterable clearable v-model="metricConfigData.endpoint" @on-change="handleEndPointChange" style="width:300px">
+              <Option v-for="item in endpointOptions" :value="item.guid" :key="item.guid">{{ item.guid }}</Option>
+            </Select>
+          </FormItem>
+          <!--预览区-->
+          <div :id="echartId" class="echart"></div>
+        </Form>
+      </div>
+      <div class="drawer-footer">
+        <Button style="margin-right: 8px" @click="handleCancel">{{ '取消' }}</Button>
+        <Button type="primary" class="primary" @click="handleSubmit">{{ '提交' }}</Button>
+      </div>
+    </Drawer>
+  </div>
+</template>
+
+<script>
+import { debounce , generateUuid} from '@/assets/js/utils'
+import { readyToDraw } from '@/assets/config/chart-rely'
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    monitorType: {
+      type: String,
+      default: ''
+    },
+    serviceGroup: {
+      type: String,
+      default: ''
+    },
+    data: {
+      type: Object,
+      default: () => {}
+    },
+    operator: {
+      type: String,
+      default: 'add'
+    }
+  },
+  data () {
+    return {
+      workspace: '', // 作用域
+      templatePl: '', // 推荐配置
+      metricTemplate: [], // 推荐配置下拉列表
+      metricTemplateParams: [],
+      collectedMetricOptions: [], // 采集数据下拉列表
+      metricConfigData: {
+        guid: null,
+        metric: '', // 名称
+        monitor_type: '', // 类型
+        panel_id: null,
+        prom_expr: '', // 表达式
+        endpoint: '' // 对象
+      },
+      endpoint: '',
+      endpointOptions: [],
+      echartId: '',
+      maxHeight: 500
+    }
+  },
+  computed: {
+    drawerVisible: {
+      get () {
+        return this.visible
+      },
+      set (val) {
+        this.$emit('update:visible', val)
+      }
+    }
+  },
+  watch: {
+    workspace (val) {
+      if (val) {
+        this.getMetricTemplate()
+      }
+    }
+  },
+  mounted () {
+    this.getEndpoint()
+    this.initDrawerHeight()
+    if (this.operator === 'edit') {
+      this.workspace = this.data.workspace
+      this.metricConfigData = {
+        ...this.data
+      }
+    }
+  },
+  methods: {
+    initDrawerHeight () {
+      this.maxHeight = document.body.clientHeight - 150
+      window.addEventListener(
+        'resize',
+        debounce(() => {
+          this.maxHeight = document.body.clientHeight - 150
+        }, 100)
+      )
+    },
+    // 选择作用域
+    changeWorkspace () {
+      this.templatePl = ''
+      this.metricTemplateParams = []
+      this.metricConfigData.prom_expr = ''
+    },
+    // 选择推荐配置
+    changeTemplatePl (val) {
+      const find = this.metricTemplate.find(item => item.prom_expr === val)
+      this.metricTemplateParams = find && find.param.split(',').map(p => {
+        return {
+          value: '',
+          label: p
+        }
+      })
+      if (find && find.name === 'custom') {
+        // this.metricConfigData.prom_expr += val
+      } else {
+        this.metricConfigData.prom_expr = val
+      }
+    },
+    // 清空推荐配置
+    clearTemplatePl () {
+      this.templatePl = ''
+      this.metricConfigData.endpoint = ''
+      this.metricTemplateParams = []
+    },
+    // 获取推荐配置列表
+    getMetricTemplate () {
+      const params = {
+        workspace: this.workspace
+      }
+      this.$root.$httpRequestEntrance.httpRequestEntrance(
+        'GET',
+        '/monitor/api/v2/sys/parameter/metric_template',
+        params,
+        res => {
+          this.metricTemplate = res
+        },
+        { isNeedloading: false }
+      )
+    },
+    // 选择采集数据
+    changeCollectedMetric (val) {
+      if (val.value) {
+        const find = this.metricTemplate.find(m => m.prom_expr === this.templatePl)
+        if (find && find.name === 'custom') {
+          this.metricConfigData.prom_expr += val.value 
+        }
+        this.metricConfigData.prom_expr = this.metricConfigData.prom_expr.replaceAll('undefined', '')
+        this.metricConfigData.prom_expr = this.metricConfigData.prom_expr.replaceAll(val.label, val.value)
+      }
+    },
+    // 获取采集数据列表
+    getCollectedMetric () {
+      const params = {
+        monitor_type: this.monitorType,
+        guid: this.endpoint,
+        service_group: this.serviceGroup,
+        workspace: this.workspace
+      }
+      this.$root.$httpRequestEntrance.httpRequestEntrance(
+        'POST',
+        this.$root.apiCenter.getMetricOptions,
+        params,
+        responseData => {
+          this.collectedMetricOptions = responseData
+        },
+        { isNeedloading: false }
+      )
+    },
+    // 获取预览对象列表
+    getEndpoint () {
+      const params = {
+        type: this.monitorType,
+        serviceGroup: this.serviceGroup
+      }
+      this.$root.$httpRequestEntrance.httpRequestEntrance(
+        'GET',
+        this.$root.apiCenter.getEndpoint,
+        params,
+        responseData => {
+          this.endpointOptions = responseData
+        })
+    },
+    // 选择预览对象
+    handleEndPointChange (val) {
+      if (!val) return
+      this.getChartData()
+      // if (this.workspace === 'all_object') {
+      //   this.getChartData()
+      // } else {
+      //   this.getEndpoint()
+      // }
+    },
+    // 渲染echart
+    async getChartData () {
+      generateUuid().then((elId)=>{
+        this.echartId = `id_${elId}`
+      })
+      let params = {
+        aggregate: 'none',
+        time_second: -1800,
+        start: 0,
+        end: 0,
+        title: '',
+        unit: '',
+        data: []
+      }
+      const find = this.endpointOptions.find(e => e.guid === this.metricConfigData.endpoint)
+      params.data = [{
+        endpoint: (find && find.guid) || '',
+        app_object: this.serviceGroup,
+        endpoint_type: this.monitorType,
+        prom_ql: this.metricConfigData.prom_expr,
+        metric: this.metricConfigData.prom_expr === '' ? this.metricConfigData.metric : ''
+      }]
+      this.$root.$httpRequestEntrance.httpRequestEntrance(
+        'POST',
+        this.$root.apiCenter.metricConfigView.api,
+        params,
+        responseData => {
+          const chartConfig = {
+            eye: false,
+            clear: true
+          }
+          readyToDraw(this, responseData, 1, chartConfig, this.echartId)
+        },
+        { isNeedloading: false }
+      )
+    },
+    handleSubmit () {
+      const type = !this.metricConfigData.guid ? 'POST' : 'PUT'
+      this.metricConfigData.monitor_type = this.monitorType
+      this.metricConfigData.service_group = this.serviceGroup
+      this.metricConfigData.workspace = this.workspace
+      this.$root.$httpRequestEntrance.httpRequestEntrance(
+        type,
+        this.$root.apiCenter.metricManagement,
+        [this.metricConfigData],
+        () => {
+          this.$Message.success(this.$t('tips.success'))
+          this.$emit('update:visible', false)
+          this.$emit('fetchList')
+        })
+    },
+    handleCancel () {
+      this.$emit('update:visible', false)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.monitor-add-group {
+  .w-header {
+    display: flex;
+    align-items: center;
+    .title {
+      font-size: 16px;
+      font-weight: bold;
+      color: #515a6e;
+      margin: 0 6px;
+      width: fit-content;
+      .underline {
+        display: block;
+        margin-top: -12px;
+        margin-left: -8px;
+        width: 100%;
+        padding: 0 8px;
+        height: 12px;
+        border-radius: 12px;
+        background-color: #c6eafe;
+        box-sizing: content-box;
+      }
+    }
+  }
+  .content {
+    padding: 8px;
+    overflow-y: auto;
+    .echart {
+      width: 100%;
+      height: 280px;
+    }
+  }
+  .drawer-footer {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    border-top: 1px solid #e8e8e8;
+    padding: 10px 16px;
+    text-align: center;
+    background: #fff;
+  }
+  /deep/.ivu-form-item {
+    margin-bottom: 8px;
+  }
+}
+</style>
