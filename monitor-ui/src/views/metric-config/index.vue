3756c60b91741b02799df44393f52f9488549dd5
wanghao7717 <792974788@qq.com>
2024-05-23 20:58:19 +0800

    feat: 指标配置页面重构

---
+++ monitor-ui/src/views/metric-config/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="monitor-metric-config">
+    <Tabs v-model="activeName">
+      <TabPane label="通用对象" name="1">
+      </TabPane>
+      <TabPane label="层级对象" name="2">
+      </TabPane>
+    </Tabs>
+    <GeneralGroup v-if="activeName === '1'"></GeneralGroup>
+    <LevelGroup v-if="activeName === '2'"></LevelGroup>
+  </div>
+</template>
+
+<script>
+import GeneralGroup from './general-group.vue'
+import LevelGroup from './level-group.vue'
+export default {
+  components: {
+    GeneralGroup,
+    LevelGroup
+  },
+  data () {
+    return {
+      activeName: '1'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.monitor-metric-config {
+  width: 100%;
+}
+</style>
