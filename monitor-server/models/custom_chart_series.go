3dd21d8b1e89cf76633a2a365cd9b16b9c39dadf
tangjiawei <tangjiawei@nnyy.com>
2024-05-24 15:26:27 +0800

    【自定义视图】custom_chart_series 添加饼图展示标签

--- monitor-server/models/custom_chart_series.go
+++ monitor-server/models/custom_chart_series.go
@@ -9,17 +9,19 @@ type CustomChartSeries struct {
 	MonitorType    string `json:"monitorType" xorm:"monitor_type"`       // 监控类型
 	Metric         string `json:"metric" xorm:"metric"`                  // 指标
 	ColorGroup     string `json:"colorGroup" xorm:"color_group"`         // 默认色系
+	PieDisplayTag  string `json:"pieDisplayTag" xorm:"pie_display_tag"`  // 饼图展示标签
 }
 
 type CustomChartSeriesDto struct {
-	Endpoint     string            `json:"endpoint"`      // 监控对象
-	ServiceGroup string            `json:"serviceGroup"`  // 层级对象
-	EndpointName string            `json:"endpointName" ` // 层级对象
-	MonitorType  string            `json:"monitorType" `  // 监控类型
-	ColorGroup   string            `json:"colorGroup" `   // 默认色系
-	Metric       string            `json:"metric"`        // 指标
-	Tags         []*TagDto         `json:"tags"`          // 标签
-	ColorConfig  []*ColorConfigDto `json:"series"`        // 颜色
+	Endpoint      string            `json:"endpoint"`      // 监控对象
+	ServiceGroup  string            `json:"serviceGroup"`  // 层级对象
+	EndpointName  string            `json:"endpointName" ` // 层级对象
+	MonitorType   string            `json:"monitorType" `  // 监控类型
+	ColorGroup    string            `json:"colorGroup" `   // 默认色系
+	PieDisplayTag string            `json:"pieDisplayTag"` // 饼图展示标签
+	Metric        string            `json:"metric"`        // 指标
+	Tags          []*TagDto         `json:"tags"`          // 标签
+	ColorConfig   []*ColorConfigDto `json:"series"`        // 颜色
 }
 
 type TagDto struct {
