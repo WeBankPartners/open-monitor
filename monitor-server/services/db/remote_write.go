8ee2ae915dac79fb3e11e3c07a51462dc93c96d9
zgyzgyhero <894526647@qq.com>
2024-06-03 10:22:54 +0800

    add remote write config

--- monitor-server/services/db/remote_write.go
+++ monitor-server/services/db/remote_write.go
@@ -3,6 +3,9 @@ package db
 import (
 	"fmt"
 	"github.com/WeBankPartners/open-monitor/monitor-server/models"
+	"github.com/WeBankPartners/open-monitor/monitor-server/services/prom"
+	"io/ioutil"
+	"strings"
 	"time"
 )
 
@@ -49,6 +52,46 @@ func RemoteWriteConfigDelete(id string) error {
 }
 
 func SyncRemoteWritePrometheusConfig() error {
-
+	var remoteWriteConfigRows []*models.RemoteWriteConfigTable
+	err := x.SQL("select * from remote_write_config where address<>''").Find(&remoteWriteConfigRows)
+	if err != nil {
+		return fmt.Errorf("Try to get remote write table fail,%s ", err.Error())
+	}
+	backupConfigName, backupErr := backupPrometheusConfig()
+	if backupErr != nil {
+		return backupErr
+	}
+	promBytes, err := ioutil.ReadFile("/app/monitor/prometheus/prometheus.yml")
+	if err != nil {
+		return fmt.Errorf("Read prometheus tpl file fail,%s ", err.Error())
+	}
+	promString := string(promBytes)
+	startIndex := strings.Index(promString, "#Remote_write_start")
+	endIndex := strings.Index(promString, "#Remote_write_end")
+	tplBytes, err := ioutil.ReadFile("/app/monitor/prometheus/remote_write_prometheus.tpl")
+	if err != nil {
+		return fmt.Errorf("Read remote write prometheus tpl file fail,%s ", err.Error())
+	}
+	remoteWriteConfigString := ""
+	tplString := string(tplBytes)
+	for _, row := range remoteWriteConfigRows {
+		tmpConfigString := tplString + "\n"
+		tmpConfigString = strings.ReplaceAll(tmpConfigString, "{{remote_write_url}}", row.Address)
+		remoteWriteConfigString += tmpConfigString + "\n"
+	}
+	promString = promString[:startIndex+11] + "\n" + remoteWriteConfigString + promString[endIndex:]
+	err = ioutil.WriteFile("/app/monitor/prometheus/prometheus.yml", []byte(promString), 0644)
+	if err != nil {
+		err = fmt.Errorf("Write remote write config to prometheus fail,%s ", err.Error())
+		recoverPrometheusConfig(backupConfigName)
+		return err
+	}
+	err = prom.ReloadConfig()
+	if err != nil {
+		err = fmt.Errorf("Reload prometheus config fail,%s ", err.Error())
+		recoverPrometheusConfig(backupConfigName)
+		prom.ReloadConfig()
+		return err
+	}
 	return nil
 }
