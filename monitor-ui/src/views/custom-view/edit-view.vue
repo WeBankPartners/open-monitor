53bf295196c718111172999f86ac8b0111ad63a0
Alexaa123 <1239327906@qq.com>
2024-06-07 11:41:22 +0800

    解决现有问题6

--- monitor-ui/src/views/custom-view/edit-view.vue
+++ monitor-ui/src/views/custom-view/edit-view.vue
@@ -1114,6 +1114,7 @@ export default {
     drawChartContent() {
       if (this.isPieChart) {
         const params = this.generateLineParamsData();
+        params[0].pieType = this.chartConfigForm.pieType;
         if (!params[0].metric) return;
         this.request('POST', '/monitor/api/v1/dashboard/pie/chart', params,
           res => {
