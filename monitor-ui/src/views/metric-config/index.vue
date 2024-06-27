985d41a4f09f047cca4f5a0de211219b41c472f7
pobu168 <244202969@qq.com>
2024-06-27 09:49:20 +0800

    add: tmp

--- monitor-ui/src/views/metric-config/index.vue
+++ monitor-ui/src/views/metric-config/index.vue
@@ -7,19 +7,31 @@
       <!--层级对象-->
       <TabPane :label="$t('m_field_resourceLevel')" name="2">
       </TabPane>
+      <!--对象组-->
+      <TabPane :label="$t('m_object_group')" name="3">
+      </TabPane>
+      <!--对象-->
+      <TabPane :label="$t('m_object_design')+'('+$t('m_ready_only')+')'" name="4">
+      </TabPane>
     </Tabs>
     <GeneralGroup v-if="activeName === '1'"></GeneralGroup>
     <LevelGroup v-if="activeName === '2'"></LevelGroup>
+    <ObjectGroup v-if="activeName === '3'"></ObjectGroup>
+    <Object v-if="activeName === '4'"></Object>
   </div>
 </template>
 
 <script>
 import GeneralGroup from './general-group.vue'
 import LevelGroup from './level-group.vue'
+import ObjectGroup from './object-group.vue'
+import Object from './object.vue'
 export default {
   components: {
     GeneralGroup,
-    LevelGroup
+    LevelGroup,
+    ObjectGroup,
+    Object
   },
   data () {
     return {
