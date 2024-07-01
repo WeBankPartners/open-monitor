dfe6f2cc73009d83609daddd5222c483ea22e19a
tangjiawei <tangjiawei@nnyy.com>
2024-07-01 15:59:44 +0800

    添加同环比编辑接口

--- monitor-server/models/endpoint.go
+++ monitor-server/models/endpoint.go
@@ -64,6 +64,7 @@ type MetricTable struct {
 	LogMetricConfig    string `json:"log_metric_config" xorm:"log_metric_config"`
 	LogMetricTemplate  string `json:"log_metric_template" xorm:"log_metric_template"`
 	LogMetricGroup     string `json:"log_metric_group" xorm:"log_metric_group"`
+	EndpointGroup      string `json:"endpoint_group" xorm:"endpoint_group"`
 	MetricType         string `json:"metric_type" xorm:"-"`           // 指标类型
 	LogMetricGroupName string `json:"log_metric_group_name" xorm:"-"` // 配置的模版名
 }
@@ -88,12 +89,13 @@ type MetricComparisonExtend struct {
 	LogMetricConfig    string   `json:"log_metric_config" xorm:"log_metric_config"`
 	LogMetricTemplate  string   `json:"log_metric_template" xorm:"log_metric_template"`
 	LogMetricGroup     string   `json:"log_metric_group" xorm:"log_metric_group"`
-	MetricType         string   `json:"metric_type" xorm:"-"`                  // 指标类型
-	LogMetricGroupName string   `json:"log_metric_group_name" xorm:"-"`        // 配置的模版名
-	PromExpr           string   `json:"promExpr" xorm:"prom_expr"`             // 同环比指标prom表达式
-	OriginCalcType     string   `json:"-" xorm:"calc_type"`                    // 计算数值: diff 差值,diff_percent 差值百分比,可以多选,逗号隔开
-	CalcType           []string `json:"calcType" xorm:"-"`                     // 计算数值: diff 差值,diff_percent 差值百分比,数组
-	CalcMethod         string   `json:"calcMethod" xorm:"calc_method"`         // 计算方法: avg平均,sum求和
-	CalcPeriod         int      `json:"calcPeriod" xorm:"calc_period"`         // 计算周期
-	ComparisonType     string   `json:"comparisonType" xorm:"comparison_type"` // 对比类型: day 日环比, week 周, 月周比 month
+	MetricType         string   `json:"metric_type" xorm:"-"`                           // 指标类型
+	LogMetricGroupName string   `json:"log_metric_group_name" xorm:"-"`                 // 配置的模版名
+	MetricComparisonId string   `json:"metricComparisonId" xorm:"metric_comparison_id"` // 同环比指标Id
+	PromExpr           string   `json:"promExpr" xorm:"prom_expr"`                      // 同环比指标prom表达式
+	OriginCalcType     string   `json:"-" xorm:"calc_type"`                             // 计算数值: diff 差值,diff_percent 差值百分比,可以多选,逗号隔开
+	CalcType           []string `json:"calcType" xorm:"-"`                              // 计算数值: diff 差值,diff_percent 差值百分比,数组
+	CalcMethod         string   `json:"calcMethod" xorm:"calc_method"`                  // 计算方法: avg平均,sum求和
+	CalcPeriod         int      `json:"calcPeriod" xorm:"calc_period"`                  // 计算周期
+	ComparisonType     string   `json:"comparisonType" xorm:"comparison_type"`          // 对比类型: day 日环比, week 周, 月周比 month
 }
