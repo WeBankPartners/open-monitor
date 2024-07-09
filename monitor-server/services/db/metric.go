3abc505ec3f0f9a715c84a1123c2fe9d46802fb8
tangjiawei <tangjiawei@nnyy.com>
2024-07-09 20:11:10 +0800

    同环比列表查询优化

--- monitor-server/services/db/metric.go
+++ monitor-server/services/db/metric.go
@@ -180,7 +180,7 @@ func MetricComparisonListNew(guid, monitorType, serviceGroup, onlyService, endpo
 			baseSql = baseSql + ") m join metric_comparison mc on mc.metric_id = m.guid"
 			params = []interface{}{endpoint, endpoint, endpoint}
 		} else {
-			baseSql = "select m.*,mc.guid as metric_comparison_id,mc.comparison_type,mc.calc_type,mc.calc_method,mc.calc_period,mc.origin_metric_id as metric_id,,mc.origin_metric from metric m join metric_comparison mc on mc.metric_id = m.guid  and  m.monitor_type=? and m.service_group is null"
+			baseSql = "select m.*,mc.guid as metric_comparison_id,mc.comparison_type,mc.calc_type,mc.calc_method,mc.calc_period,mc.origin_metric_id as metric_id,mc.origin_metric from metric m join metric_comparison mc on mc.metric_id = m.guid  and  m.monitor_type=? and m.service_group is null"
 			params = []interface{}{monitorType}
 		}
 	}
