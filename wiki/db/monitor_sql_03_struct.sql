daeb9ad0b168e50d892109ab9dac29445570fa3a
tangjiawei <tangjiawei@nnyy.com>
2024-07-15 19:05:52 +0800

    告警列表查询调整

--- wiki/db/monitor_sql_03_struct.sql
+++ wiki/db/monitor_sql_03_struct.sql
@@ -66,4 +66,7 @@ alter table log_keyword_config add column create_time varchar(32) default null;
 alter table endpoint_new add column create_user varchar(64) default null COMMENT '创建人';
 alter table endpoint_new add column update_user varchar(64) default null COMMENT '更新人';
 
+alter table endpoint_group add column create_user varchar(64) default null COMMENT '创建人';
+alter table endpoint_group add column update_user varchar(64) default null COMMENT '更新人';
+
 alter table role ADD UNIQUE (name);
