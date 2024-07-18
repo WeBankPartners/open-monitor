8632dc548a385ef2ec7534970047b79f89d58ca4
Alexaa123 <1239327906@qq.com>
2024-07-18 21:15:26 +0800

    解决报警列表问题9

--- monitor-ui/src/assets/js/gridUtils.ts
+++ monitor-ui/src/assets/js/gridUtils.ts
@@ -1,12 +1,15 @@
 const echarts = require('echarts/lib/echarts')
-export const resizeEvent = function(that: any, i: string, newH: number, newW: number, newHPx: number, newWPx: number){
+export const resizeEvent = function (that: any, i: string, newH: number, newW: number, newHPx: number, newWPx: number){
   that.layoutData.forEach((item: any,index: number) => {
     if (item.i === i) {
       that.layoutData[index].h = newH
       that.layoutData[index].w = newW
       that.layoutData[index]._activeCharts[0].style = ''
       const myChart = echarts.init(document.getElementById(item.id))
-      myChart.resize({height:newHPx - 50+'px',width:newWPx - 6 +'px'})
+      myChart.resize({
+        height: newHPx - 50+'px',
+        width: newWPx - 6 +'px'
+      })
       return
     }
   })
