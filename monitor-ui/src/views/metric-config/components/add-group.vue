e46f135a55f313f821ee058843cf0496676549d2
pobu168 <244202969@qq.com>
2024-07-19 12:36:25 +0800

    fix: metric view &edit change bug

--- monitor-ui/src/views/metric-config/components/add-group.vue
+++ monitor-ui/src/views/metric-config/components/add-group.vue
@@ -183,6 +183,7 @@ export default {
       }
       this.metricConfigData.endpoint_type = this.metricConfigData.monitor_type
     }
+    this.getMetricTemplate()
   },
   methods: {
     setPreviewObject(obj) {
