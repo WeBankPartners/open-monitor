796091ad305e772ce4759cebf09d7f234d97af4b
pobu168 <244202969@qq.com>
2024-07-19 17:20:16 +0800

    fix: add yoy

--- monitor-ui/src/views/metric-config/level-group.vue
+++ monitor-ui/src/views/metric-config/level-group.vue
@@ -484,6 +484,7 @@ export default {
       this.showDrawer = 'comparisonMetrics'
       this.addVisible = true
       this.type = 'add'
+      this.viewOnly = false
       this.originalMetricsId = row.guid
     },
   }
