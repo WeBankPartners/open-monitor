1f1262b40ad0f17264bbd53d05ad73d6fd1297f2
zgyzgyhero <894526647@qq.com>
2024-07-29 14:52:39 +0800

    update db keyword scrape

--- monitor-server/services/db/db_keyword.go
+++ monitor-server/services/db/db_keyword.go
@@ -3,7 +3,11 @@ package db
 import (
 	"fmt"
 	"github.com/WeBankPartners/go-common-lib/guid"
+	"github.com/WeBankPartners/open-monitor/monitor-server/middleware/log"
 	"github.com/WeBankPartners/open-monitor/monitor-server/models"
+	"github.com/WeBankPartners/open-monitor/monitor-server/services/datasource"
+	"net/http"
+	"strings"
 	"time"
 )
 
@@ -220,4 +224,109 @@ func getSimpleDbKeywordConfig(dbKeywordConfigGuid string, emptyCheck bool) (resu
 		result = queryRows[0]
 	}
 	return
+}
+
+func StartDbKeywordMonitorCronJob() {
+	t := time.NewTicker(10 * time.Second).C
+	for {
+		<-t
+		go doDbKeywordMonitorJob()
+	}
+}
+
+func doDbKeywordMonitorJob() {
+	http.DefaultClient.CloseIdleConnections()
+	dataMap, err := datasource.QueryLogKeywordData()
+	if err != nil {
+		log.Logger.Error("Check log keyword break with get prometheus data", log.Error(err))
+		return
+	}
+	if len(dataMap) == 0 {
+		return
+	}
+	var logKeywordConfigs []*models.LogKeywordCronJobQuery
+	x.SQL("select t1.guid,t1.service_group,t1.log_path,t1.monitor_type,t2.keyword,t2.notify_enable,t2.priority,t2.content,t2.name,t3.source_endpoint,t3.target_endpoint,t4.agent_address from log_keyword_monitor t1 left join log_keyword_config t2 on t1.guid=t2.log_keyword_monitor left join log_keyword_endpoint_rel t3 on t1.guid=t3.log_keyword_monitor left join endpoint_new t4 on t3.source_endpoint=t4.guid where t3.source_endpoint is not null").Find(&logKeywordConfigs)
+	if len(logKeywordConfigs) == 0 {
+		log.Logger.Debug("Check log keyword break with empty config ")
+		return
+	}
+	var alarmTable []*models.LogKeywordAlarmTable
+	err = x.SQL("select * from log_keyword_alarm").Find(&alarmTable)
+	if err != nil {
+		log.Logger.Error("Check log keyword break with query exist closed alarm fail", log.Error(err))
+		return
+	}
+	alarmMap := make(map[string]*models.LogKeywordAlarmTable)
+	for _, v := range alarmTable {
+		if _, b := alarmMap[v.Tags]; b {
+			continue
+		}
+		alarmMap[v.Tags] = v
+	}
+	var addAlarmRows []*models.AlarmTable
+	var newValue, oldValue float64
+	notifyMap := make(map[string]string)
+	nowTime := time.Now()
+	for _, config := range logKeywordConfigs {
+		key := fmt.Sprintf("e_guid:%s^t_guid:%s^file:%s^keyword:%s", config.SourceEndpoint, config.TargetEndpoint, config.LogPath, config.Keyword)
+		newValue, oldValue = 0, 0
+		if dataValue, b := dataMap[key]; b {
+			newValue = dataValue
+		} else {
+			continue
+		}
+		if newValue == 0 {
+			continue
+		}
+		addFlag := false
+		if existAlarm, b := alarmMap[key]; b {
+			if existAlarm.EndValue > 0 {
+				oldValue = existAlarm.EndValue
+			} else {
+				oldValue = existAlarm.StartValue
+			}
+			if newValue == oldValue {
+				continue
+			}
+			if existAlarm.Status == "firing" {
+				existAlarm.Content = strings.Split(existAlarm.Content, "^^")[0] + "^^" + getLogKeywordLastRow(config.AgentAddress, config.LogPath, config.Keyword)
+				addAlarmRows = append(addAlarmRows, &models.AlarmTable{Id: existAlarm.AlarmId, Status: existAlarm.Status, EndValue: newValue, Content: existAlarm.Content, End: nowTime})
+			} else {
+				addFlag = true
+			}
+		} else {
+			addFlag = true
+		}
+		if addFlag {
+			if config.NotifyEnable > 0 {
+				notifyMap[key] = config.ServiceGroup
+			}
+			alarmContent := config.Content
+			if alarmContent != "" {
+				alarmContent = alarmContent + "<br/>"
+			}
+			addAlarmRows = append(addAlarmRows, &models.AlarmTable{StrategyId: 0, Endpoint: config.TargetEndpoint, Status: "firing", SMetric: "log_monitor", SExpr: "node_log_monitor_count_total", SCond: ">0", SLast: "10s", SPriority: config.Priority, Content: alarmContent + getLogKeywordLastRow(config.AgentAddress, config.LogPath, config.Keyword), Tags: key, StartValue: newValue, Start: nowTime, AlarmName: config.Name})
+		}
+	}
+	if len(addAlarmRows) == 0 {
+		return
+	}
+	for _, v := range addAlarmRows {
+		if tmpErr := doLogKeywordDBAction(v); tmpErr != nil {
+			log.Logger.Error("Update log keyword alarm table fail", log.String("tags", v.Tags), log.Error(tmpErr))
+		} else {
+			if v.Id <= 0 {
+				if _, b := notifyMap[v.Tags]; !b {
+					log.Logger.Warn("Log keyword monitor notify disable,ignore", log.String("tags", v.Tags))
+					continue
+				}
+				tmpAlarmObj := getSimpleAlarmByLogKeywordTags(v.Tags)
+				if tmpAlarmObj.Id <= 0 {
+					log.Logger.Warn("Log keyword monitor notify fail,query alarm with tags fail", log.String("tags", v.Tags))
+					continue
+				}
+				NotifyServiceGroup(notifyMap[v.Tags], &models.AlarmHandleObj{AlarmTable: tmpAlarmObj})
+			}
+		}
+	}
 }
