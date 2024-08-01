096ebe95871fc0edb64408615a8fe1ad5ab0f587
pobu168 <244202969@qq.com>
2024-08-01 18:54:40 +0800

    reset config

--- build/build-ui.sh
+++ build/build-ui.sh
@@ -4,10 +4,10 @@ npm -v
 if [ $? -eq 0 ]
 then
     cd $1/monitor-ui
-    # npm install --registry https://registry.npmmirror.com --unsafe-perm --force
+    npm install --registry https://registry.npmmirror.com --unsafe-perm --force
     # npm install
-    # npm run build
-    # npm run plugin
+    npm run build
+    npm run plugin
 else
     docker run --rm -v $1:/app/open-monitor --name node-build node:12.13.1 /bin/bash /app/open-monitor/build/build-ui-docker.sh
 fi
