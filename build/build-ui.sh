7a86b85bf451f39d3ca9e86691797458784b0192
Alexaa123 <1239327906@qq.com>
2024-08-01 10:50:45 +0800

    解决图表报错问题

--- build/build-ui.sh
+++ build/build-ui.sh
@@ -4,6 +4,7 @@ npm -v
 if [ $? -eq 0 ]
 then
     cd $1/monitor-ui
+    npm install --registry https://registry.npmmirror.com --unsafe-perm --force
     # npm install
     npm run build
     npm run plugin
