540b0937647cd65055fffb8ed648685952ed860e
pobu168 <244202969@qq.com>
2024-08-01 16:48:29 +0800

    update: makefile

--- build/build-ui.sh
+++ build/build-ui.sh
@@ -6,8 +6,8 @@ then
     cd $1/monitor-ui
     # npm install --registry https://registry.npmmirror.com --unsafe-perm --force
     # npm install
-    npm run build
-    npm run plugin
+    # npm run build
+    # npm run plugin
 else
     docker run --rm -v $1:/app/open-monitor --name node-build node:12.13.1 /bin/bash /app/open-monitor/build/build-ui-docker.sh
 fi
