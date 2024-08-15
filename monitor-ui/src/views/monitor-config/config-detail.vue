e717a9e88e4beb2e54c0f6453f2dde8832b9a3cd
pobu168 <244202969@qq.com>
2024-08-15 18:08:31 +0800

    fix: threshold value mgmt

--- monitor-ui/src/views/monitor-config/config-detail.vue
+++ monitor-ui/src/views/monitor-config/config-detail.vue
@@ -591,7 +591,6 @@ export default {
             <Input
               value={params.row.thresholdValue}
               disabled={!this.isEditState}
-              maxlength="10"
               on-on-change={v => {
 
                 this.formData.conditions[params.index].thresholdValue = v.target.value
@@ -609,7 +608,6 @@ export default {
             <Input
               value={params.row.lastValue}
               disabled={!this.isEditState}
-              maxlength="10"
               on-on-change={v => {
                 this.formData.conditions[params.index].lastValue = v.target.value
               }}
