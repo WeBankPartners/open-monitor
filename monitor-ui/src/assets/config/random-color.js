5c4dfef4ef692eb08460e4a94c6586ef895130f4
Alexaa123 <1239327906@qq.com>
2024-08-21 20:17:36 +0800

    解决告警列表筛选项只能是三个

--- monitor-ui/src/assets/config/random-color.js
+++ monitor-ui/src/assets/config/random-color.js
@@ -29,20 +29,15 @@ const hslToHex = (h, s, l) => {
   let b = 0
   if (0 <= h && h < 60) {
     r = c; g = x; b = 0
-  }
-  else if (60 <= h && h < 120) {
+  } else if (60 <= h && h < 120) {
     r = x; g = c; b = 0
-  }
-  else if (120 <= h && h < 180) {
+  } else if (120 <= h && h < 180) {
     r = 0; g = c; b = x
-  }
-  else if (180 <= h && h < 240) {
+  } else if (180 <= h && h < 240) {
     r = 0; g = x; b = c
-  }
-  else if (240 <= h && h < 300) {
+  } else if (240 <= h && h < 300) {
     r = x; g = 0; b = c
-  }
-  else if (300 <= h && h < 360) {
+  } else if (300 <= h && h < 360) {
     r = c; g = 0; b = x
   }
   r = Math.round((r + m) * 255).toString(16)
