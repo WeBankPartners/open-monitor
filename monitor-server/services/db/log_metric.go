45f9c77a0154fb9101677afb3e17d6a96d7766f1
tangjiawei <tangjiawei@nnyy.com>
2024-08-22 20:42:44 +0800

    自动生成告警指标阈值

--- monitor-server/services/db/log_metric.go
+++ monitor-server/services/db/log_metric.go
@@ -1151,11 +1151,6 @@ func ImportLogMetricExcel(logMonitorGuid, operator string, param []*models.LogMe
 	return
 }
 
-func GetLogMetricByServiceGroupNew(serviceGroup string) (result models.LogMetricQueryObj, err error) {
-
-	return
-}
-
 func GetSimpleLogMetricGroup(logMetricGroupGuid string) (result *models.LogMetricGroup, err error) {
 	var logMetricGroupRows []*models.LogMetricGroup
 	err = x.SQL("select * from log_metric_group where guid=?", logMetricGroupGuid).Find(&logMetricGroupRows)
@@ -1267,153 +1262,10 @@ func getCreateLogMetricGroupActions(param *models.LogMetricGroupWithTemplate, op
 		actions = append(actions, &Action{Sql: "insert into metric(guid,metric,monitor_type,prom_expr,service_group,workspace,update_time,log_metric_template,log_metric_group,create_time,create_user,update_user) value (?,?,?,?,?,?,?,?,?,?,?,?)",
 			Param: []interface{}{tmpMetricGuid, tmpMetricWithPrefix, monitorType, promExpr, serviceGroup, models.MetricWorkspaceService, nowTime, v.Guid, param.LogMetricGroupGuid, nowTime, operator, operator}})
 	}
-	// 自动创建告警
-	if param.AutoCreateWarn {
-		var endpointGroup string
-		var serviceGroupsRoles []string
-		codeList := getTargetCodeMap(param.CodeStringMap)
-		autoAlarmMetricList := getAutoAlarmMetricList(logMonitorTemplateObj.MetricList, serviceGroup)
-		if param.LogMetricMonitorGuid != "" {
-			var logMetricMonitor = &models.LogMetricMonitorTable{}
-			var endpointGroupIds []string
-			if _, err = x.SQL("select service_group,monitor_type from log_metric_monitor where guid=?", param.LogMetricMonitorGuid).Get(&logMetricMonitor); err != nil {
-				return
-			}
-			if logMetricMonitor != nil {
-				serviceGroupsRoles = getServiceGroupRoles(logMetricMonitor.ServiceGroup)
-				if err = x.SQL("select guid from endpoint_group where service_group=? and monitor_type=?", logMetricMonitor.ServiceGroup, logMetricMonitor.MetricType).Find(&endpointGroupIds); err != nil {
-					return
-				}
-				if len(endpointGroupIds) > 0 {
-					endpointGroup = endpointGroupIds[0]
-				}
-			}
-		}
-		// 添加 other默认告警
-		codeList = append(codeList, "{code}")
-		for _, code := range codeList {
-			for _, alarmMetric := range autoAlarmMetricList {
-				// 添加告警配置基础信息
-				alarmStrategyParam := &models.GroupStrategyObj{NotifyList: make([]*models.NotifyObj, 0), Conditions: make([]*models.StrategyConditionObj, 0)}
-				metricTags := make([]*models.MetricTag, 0)
-				alarmStrategyParam.EndpointGroup = serviceGroup
-				alarmStrategyParam.Name = fmt.Sprintf("%s%s%s%d%s", code, alarmMetric.DisplayName, translateSymbol(alarmMetric.Operator), alarmMetric.Threshold, alarmMetric.TimeUnit)
-				alarmStrategyParam.Priority = "medium"
-				alarmStrategyParam.NotifyEnable = 1
-				alarmStrategyParam.ActiveWindow = "00:00-23:59"
-				alarmStrategyParam.EndpointGroup = endpointGroup
-				alarmStrategyParam.Content = fmt.Sprintf("%s continuing for more than%d%s", alarmStrategyParam.Name, alarmMetric.Time, alarmMetric.TimeUnit)
-				// 添加编排与通知
-				alarmStrategyParam.NotifyList = append(alarmStrategyParam.NotifyList, &models.NotifyObj{AlarmAction: "firing", NotifyRoles: serviceGroupsRoles})
-				alarmStrategyParam.NotifyList = append(alarmStrategyParam.NotifyList, &models.NotifyObj{AlarmAction: "ok", NotifyRoles: serviceGroupsRoles})
-				// 添加指标阈值
-				for _, tag := range alarmMetric.TagConfig {
-					// 标签为code,需要配置 equal和TagValue值
-					if tag == "code" {
-						// code为 other 配置not in,其他配置 in
-						if code == "{code}" {
-							metricTags = append(metricTags, &models.MetricTag{
-								TagName:  "code",
-								Equal:    "notin",
-								TagValue: codeList[:len(codeList)-1],
-							})
-						} else {
-							metricTags = append(metricTags, &models.MetricTag{
-								TagName:  "code",
-								Equal:    "in",
-								TagValue: []string{code},
-							})
-						}
-					} else {
-						metricTags = append(metricTags, &models.MetricTag{
-							TagName: tag,
-						})
-					}
-				}
-
-				alarmStrategyParam.Conditions = append(alarmStrategyParam.Conditions, &models.StrategyConditionObj{
-					Metric:     alarmMetric.MetricId,
-					MetricName: alarmMetric.Metric,
-					Condition:  fmt.Sprintf("%s%d", alarmMetric.Operator, alarmMetric.Threshold),
-					Last:       fmt.Sprintf("%d%s", alarmMetric.Time, alarmMetric.TimeUnit),
-					Tags:       metricTags,
-				})
-				if subCreateAlarmStrategyActions, err = getCreateAlarmStrategyActions(alarmStrategyParam, nowTime.Format(models.DatetimeFormat), operator); err != nil {
-					return
-				}
-				if len(subCreateAlarmStrategyActions) > 0 {
-					actions = append(actions, subCreateAlarmStrategyActions...)
-				}
-			}
-		}
+	if subCreateAlarmStrategyActions, err = autoGenerateAlarmStrategy(param, logMonitorTemplateObj.MetricList, serviceGroup, operator); err != nil {
+		actions = append(actions, subCreateAlarmStrategyActions...)
 	}
 	return
-}
-
-func generateMetricGuid(metric, serviceGroup string) string {
-	return fmt.Sprintf("%s__%s", metric, serviceGroup)
-}
-
-func getServiceGroupRoles(serviceGroup string) []string {
-	var optionModels []*models.OptionModel
-	var roles []string
-	optionModels, _ = GetOrgRole(serviceGroup)
-	if len(optionModels) == 0 {
-		return roles
-	}
-	for _, model := range optionModels {
-		roles = append(roles, model.OptionName)
-	}
-	return roles
-}
-
-// getAutoAlarmMetricList 获取自动告警的指标列表
-func getAutoAlarmMetricList(list []*models.LogMetricTemplate, serviceGroup string) []*models.LogMetricThreshold {
-	var metricThresholdList []*models.LogMetricThreshold
-	if len(list) == 0 {
-		return metricThresholdList
-	}
-	for _, logMetricTemplate := range list {
-		if logMetricTemplate.AutoAlarm && logMetricTemplate.RangeConfig != "" {
-			temp := &models.ThresholdConfig{}
-			json.Unmarshal([]byte(logMetricTemplate.RangeConfig), temp)
-			metricThresholdList = append(metricThresholdList, &models.LogMetricThreshold{
-				MetricId:        generateMetricGuid(logMetricTemplate.Metric, serviceGroup),
-				Metric:          logMetricTemplate.Metric,
-				DisplayName:     logMetricTemplate.DisplayName,
-				ThresholdConfig: temp,
-				TagConfig:       logMetricTemplate.TagConfigList,
-			})
-		}
-	}
-	return metricThresholdList
-}
-
-// getTargetCodeMap 获取配置的目标code集合
-func getTargetCodeMap(codeList []*models.LogMetricStringMapTable) []string {
-	var list []string
-	if len(codeList) == 0 {
-		return []string{}
-	}
-	for _, table := range codeList {
-		list = append(list, table.TargetValue)
-	}
-	return list
-}
-
-// translateSymbol 字符翻译
-func translateSymbol(operator string) string {
-	switch operator {
-	case ">":
-		return "greater than"
-	case ">=":
-		return "greater than or equal"
-	case "<":
-		return "less than"
-	case "<=":
-		return "less than or equal"
-	}
-	return ""
 }
 
 func getCreateLogMetricGroupMapAction(param *models.LogMetricGroupWithTemplate, nowTime time.Time) (sucRetCode string, actions []*Action) {
@@ -1712,30 +1564,6 @@ func CreateLogMetricCustomGroup(param *models.LogMetricGroupObj, operator string
 		return
 	}
 	err = Transaction(actions)
-	return
-}
-
-func CopyLogMetricCustomGroup(guid string) (logMetricMonitor string, err error) {
-	return
-}
-
-func GetLogMetricGroupObj(logMetricGroupId string) (logMetricGroupObj models.LogMetricGroupObj, err error) {
-	var logMetricGroup models.LogMetricGroup
-	var serviceGroup, monitorType string
-	if _, err = x.SQL("select * from log_metric_group where guid= ?", logMetricGroupId).Get(&logMetricGroup); err != nil {
-		return
-	}
-	logMetricGroup.Name = logMetricGroup.Name + "1"
-	serviceGroup, monitorType = GetLogMetricServiceGroup(logMetricGroup.LogMetricMonitor)
-	logMetricGroupObj = models.LogMetricGroupObj{
-		LogMetricGroup:         logMetricGroup,
-		LogMonitorTemplateName: logMetricGroup.LogMonitorTemplate,
-		ServiceGroup:           serviceGroup,
-		MonitorType:            monitorType,
-		JsonRegular:            "",
-		ParamList:              nil,
-		MetricList:             nil,
-	}
 	return
 }
 
