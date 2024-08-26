d77e247b0f85581774cea3696c3f54e0deb49bdb
tangjiawei <tangjiawei@nnyy.com>
2024-08-26 16:16:48 +0800

    图表添加添加源看板名称返回

--- monitor-server/models/custom_chart.go
+++ monitor-server/models/custom_chart.go
@@ -62,6 +62,7 @@ type ChartSharedDto struct {
 	Id              string `json:"id"`
 	SourceDashboard int    `json:"sourceDashboard"` // 源看板
 	Name            string `json:"name"`            // 图表名称
+	DashboardName   string `json:"dashboardName"`   // 看板名称
 }
 
 type ChartSharedParam struct {
