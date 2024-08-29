1827b6a9b179d7644ce76fcbb1f52b10757ac5cb
zgyzgyhero <894526647@qq.com>
2024-08-29 16:42:53 +0800

    update alarm firing action

--- wiki/db/monitor_sql_02_base_data_en.sql
+++ wiki/db/monitor_sql_02_base_data_en.sql
@@ -1164,4 +1164,31 @@ alter table metric add column db_metric_monitor varchar(64) default null;
 alter table alarm_strategy_metric add column monitor_engine tinyint(4) default 0;
 create index idx_alarm_strategy_monitor_engine on alarm_strategy_metric(monitor_engine);
 alter table alarm_strategy_metric add column monitor_engine_expr text default null;
+
+CREATE TABLE `alarm_firing` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `endpoint` varchar(255) NOT null COMMENT '告警对象',
+    `metric` varchar(255) NOT NULL COMMENT '告警指标',
+    `tags` varchar(1024) DEFAULT '' COMMENT '告警标签',
+    `alarm_name` varchar(64) DEFAULT NULL COMMENT '告警名称',
+    `alarm_strategy` varchar(64) DEFAULT NULL COMMENT '告警策略配置',
+    `notify_id` varchar(64) DEFAULT NULL COMMENT '告警通知配置',
+    `expr` varchar(4096) DEFAULT NULL COMMENT '告警表达式',
+    `cond` varchar(50) NOT NULL COMMENT '告警条件',
+    `last` varchar(50) NOT NULL COMMENT '告警持续时间',
+    `priority` varchar(50) NOT NULL COMMENT '告警级别',
+    `content` text COMMENT '告警描述内容',
+    `start_value` double DEFAULT null COMMENT '告警发生值',
+    `start` datetime DEFAULT null COMMENT '告警发生时间',
+    `custom_message` varchar(500) DEFAULT NULL COMMENT '告警人工备注',
+    `unique_hash` varchar(64) DEFAULT NULL COMMENT '告警唯一标识(对象+指标+标签+配置)',
+    `alarm_id` int(11) DEFAULT NULL COMMENT '告警历史表id',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `unique_alarm_firing_hash` (`unique_hash`),
+    KEY `alarm_firing_endpoint_idx` (`endpoint`),
+    KEY `alarm_firing_metric_idx` (`metric`),
+    KEY `alarm_firing_priority_idx` (`priority`),
+    KEY `alarm_firing_name` (`alarm_name`),
+    KEY `alarm_firing_alarm_id` (`alarm_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 #@v3.1.4-end@;
