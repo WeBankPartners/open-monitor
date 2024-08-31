4ffd03d4c3b9247fc57d4aa96b8eb2dbb79954f2
zgyzgyhero <894526647@qq.com>
2024-08-31 15:13:11 +0800

    update keyword duplicate check

--- monitor-server/services/db/node_exporter.go
+++ monitor-server/services/db/node_exporter.go
@@ -284,7 +284,9 @@ func getLogKeywordExporterConfig(endpoint string) (result []*models.LogKeywordHt
 					if existTarget, sameFlag := existKeywordMap[logKeywordConfig.Keyword]; sameFlag {
 						if existTarget == targetEndpoint {
 							// path keyword target is same
-							continue
+							err = fmt.Errorf("Endpint:%s Path:%s keyword:%s duplicated ", endpoint, logKeywordMonitor.LogPath, logKeywordConfig.Keyword)
+							return
+							//continue
 						}
 					}
 					tmpKeywordObj := models.LogKeywordHttpRuleObj{Keyword: logKeywordConfig.Keyword, TargetEndpoint: targetEndpoint, RegularEnable: false}
@@ -310,18 +312,6 @@ func getLogKeywordExporterConfig(endpoint string) (result []*models.LogKeywordHt
 	}
 	for _, path := range pathList {
 		result = append(result, &models.LogKeywordHttpDto{Path: path, Keywords: pathMap[path]})
-	}
-	pathKeywordExistMap := make(map[string]int)
-	for _, tmpPath := range result {
-		for _, tmpKeyword := range tmpPath.Keywords {
-			duplicateKey := fmt.Sprintf("%s^^^%s", tmpPath.Path, tmpKeyword.Keyword)
-			if _, ok := pathKeywordExistMap[duplicateKey]; ok {
-				err = fmt.Errorf("Endpint:%s path:%s keyword:%s duplicated ", endpoint, tmpPath.Path, tmpKeyword.Keyword)
-				return
-			} else {
-				pathKeywordExistMap[duplicateKey] = 1
-			}
-		}
 	}
 	return
 }
