092bd170312dc9b67f2be93ee1214101d6a688a8
Alexaa123 <1239327906@qq.com>
2024-09-03 20:55:47 +0800

    关键字窗口时间数组配置

--- monitor-ui/src/views/monitor-config/business-monitor-group.vue
+++ monitor-ui/src/views/monitor-config/business-monitor-group.vue
@@ -969,8 +969,8 @@ export default {
         step: 10,
         endpoint_rel: []
       }
-      this.getEndpoint('process', 'mysql')
-      this.getEndpointDefaultValue('process')
+      this.getEndpoint('process', 'mysql', true)
+      // this.getEndpointDefaultValue('process')
       this.dbModelConfig.isAdd = true
       this.dbModelConfig.isShow = true
     },
@@ -1195,10 +1195,12 @@ export default {
         endpoint_rel: []
       }
     },
-    async getEndpoint(val, type) {
+    async getEndpoint(val, type, needGetDefault = false) {
       this.addAndEditModal.dataConfig.endpoint_rel = []
       this.dbModelConfig.addRow.endpoint_rel = []
-      await this.getDefaultConfig(val, type)
+      if (needGetDefault) {
+        await this.getDefaultConfig(val, type)
+      }
       // get source Endpoint
       const sourceApi = this.$root.apiCenter.getEndpointsByType + '/' + this.targetId + '/endpoint/' + type
       this.$root.$httpRequestEntrance.httpRequestEntrance('GET', sourceApi, '', responseData => {
@@ -1289,7 +1291,7 @@ export default {
     },
     async add() {
       this.cancelAddAndEdit()
-      this.getEndpoint(this.addAndEditModal.dataConfig.monitor_type, 'host')
+      this.getEndpoint(this.addAndEditModal.dataConfig.monitor_type, 'host', true)
       this.addAndEditModal.isAdd = true
       this.addAndEditModal.isShow = true
     },
