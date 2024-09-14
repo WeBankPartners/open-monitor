14c34dd2b7f1ed47124d57f5befb483dede4ffad
Alexaa123 <1239327906@qq.com>
2024-09-14 17:01:20 +0800

    增加自定义模板7

--- monitor-ui/src/assets/js/utils.js
+++ monitor-ui/src/assets/js/utils.js
@@ -104,3 +104,12 @@ export const showPoptipOnTable = (className='.ivu-poptip-popper') => {
 export const isStringFromNumber = str => !isNaN(str) && !isNaN(parseFloat(str))
 
 export const isPositiveNumericString = str => /^\d+$/.test(str) && parseFloat(str) >= 0
+
+export const getRandomColor = () => {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
