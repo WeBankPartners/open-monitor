d5b2bff0f642ae6e3087caef6a51af073af272e5
zgyzgyhero <894526647@qq.com>
2024-09-20 13:01:07 +0800

    update node exporter process name

--- build/build-agent.sh
+++ build/build-agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e -x
-mv monitor-agent/node_exporter/node_exporter_new build/node_exporter/
+mv monitor-agent/node_exporter/monitor_exporter build/node_exporter/
 /bin/cp -f monitor-agent/node_exporter/VERSION build/node_exporter/
 cd build
 chmod 644 node_exporter/VERSION
