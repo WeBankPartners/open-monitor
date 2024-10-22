53bcf4256c2d83f84ca601dfb528fa19334a6374
tangjiawei <tangjiawei@nnyy.com>
2024-10-22 20:14:34 +0800

    #830 custom template add string_code_map

--- monitor-server/models/log_template.go
+++ monitor-server/models/log_template.go
@@ -97,7 +97,7 @@ func (l *LogMetricTemplate) TransToLogMetric() (output *LogMetricConfigDto) {
 type LogMonitorTemplateDto struct {
 	LogMonitorTemplate
 	CalcResultObj             *CheckRegExpResult            `json:"calc_result"`
-	ParamList                 []*LogParamTemplate           `json:"param_list"`
+	ParamList                 []*LogParamTemplateObj        `json:"param_list"`
 	MetricList                []*LogMetricTemplate          `json:"metric_list"`
 	Permission                *LogMonitorTemplatePermission `json:"permission"`
 	LogMonitorTemplateVersion string                        `json:"log_monitor_template_version"`
