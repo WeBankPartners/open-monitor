cac5450ed283fd0ade74889a642625410734332b
tangjiawei <tangjiawei@nnyy.com>
2024-10-30 17:33:06 +0800

    底座迁移,添加监控名称查询

--- monitor-server/api/v1/alarm/organization.go
+++ monitor-server/api/v1/alarm/organization.go
@@ -214,3 +214,17 @@ func SearchSysPanelData(c *gin.Context) {
 	result := db.SearchPanelByName(search, endpoint)
 	mid.ReturnSuccessData(c, result)
 }
+
+func BatchGetServiceGroup(c *gin.Context) {
+	var param m.IdsParam
+	if err := c.ShouldBindJSON(&param); err != nil {
+		mid.ReturnValidateError(c, err.Error())
+		return
+	}
+	result, err := db.BatchGetServiceGroupNameByIds(param.Ids)
+	if err != nil {
+		mid.ReturnHandleError(c, err.Error(), err)
+	} else {
+		mid.ReturnSuccessData(c, result)
+	}
+}
