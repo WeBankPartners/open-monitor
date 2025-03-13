1bb7cba165d830330d53855689f19c21ebf924a1
tangjiawei <tangjiawei@nnyy.com>
2025-03-13 17:03:24 +0800

    监控告警列表调整

--- monitor-server/services/db/alarm.go
+++ monitor-server/services/db/alarm.go
@@ -1628,7 +1628,7 @@ func QueryAlarmBySql(sql string, params []interface{}, customQueryParam m.Custom
 		}
 		if v.DurationSec == 0 {
 			if v.End.IsZero() && strings.TrimSpace(v.UpdateString) != "" {
-				updateTime, _ := time.Parse(m.DatetimeFormat, v.UpdateString)
+				updateTime, _ := time.ParseInLocation(m.DatetimeFormat, v.UpdateString, time.Local)
 				v.DurationSec = int64(updateTime.Sub(v.Start).Seconds())
 			} else {
 				v.DurationSec = int64(v.End.Sub(v.Start).Seconds())
