7eed9d44aaee003b88672c422c6dea00396455df
tangjiawei <1135176001@qq.com>
2025-08-10 21:16:36 +0800

    失败数计算表达式调整

--- monitor-server/services/db/log_metric.go
+++ monitor-server/services/db/log_metric.go
@@ -582,7 +582,7 @@ func getLogMetricRatePromExpr(metric, metricPrefix, aggType, serviceGroup, sucRe
 	}
 	if metric == "req_fail_count" {
 		// 失败数：直接统计 retcode!="success" 的请求数
-		result = fmt.Sprintf("sum(%s{key=\"%sreq_count\",agg=\"%s\",service_group=\"%s\",code=\"$t_code\",retcode!=\"%s\"}) by (service_group,code)", models.LogMetricName, metricPrefix, aggType, serviceGroup, sucRetCode)
+		result = fmt.Sprintf("sum(%s{key=\"%sreq_count\",agg=\"%s\",service_group=\"%s\",code=\"$t_code\",retcode!=\"%s\"}) by (key,agg,service_group,code,retcode)", models.LogMetricName, metricPrefix, aggType, serviceGroup, sucRetCode)
 		return
 	}
 	if metric == "req_suc_rate" {
