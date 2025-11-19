2f98eb1b23d93a5d6bc57f779f828c6c3bfd6a2b
tangjiawei <1135176001@qq.com>
2025-11-19 16:21:37 +0800

    架构支持:支持x86_64和ARM64两种系统编译运行

--- build/build-monitor-server.sh
+++ build/build-monitor-server.sh
@@ -2,4 +2,4 @@
 set -e -x
 cd $(dirname $0)/../monitor-server
 export CGO_ENABLED=1
-go build -buildvcs=false -ldflags "-linkmode external -extldflags -static -s"
+go build -ldflags "-linkmode external -extldflags -static -s"
--- build/build-server.sh
+++ build/build-server.sh
@@ -3,18 +3,18 @@ set -e -x
 cd $(dirname $0)/../monitor-server
 export CGO_ENABLED=1
 cd ../monitor-agent/agent_manager
-go build -buildvcs=false  -ldflags "-linkmode external -extldflags -static -s"
+go build -ldflags "-linkmode external -extldflags -static -s"
 cd ../archive_mysql_tool
-go build -buildvcs=false  -ldflags "-linkmode external -extldflags -static -s"
+go build -ldflags "-linkmode external -extldflags -static -s"
 cd ../ping_exporter
-go build -buildvcs=false  -ldflags "-linkmode external -extldflags -static -s"
+go build -ldflags "-linkmode external -extldflags -static -s"
 cd ../node_exporter
-go build -buildvcs=false  -ldflags "-linkmode external -extldflags -static -s" -o monitor_exporter
+go build -ldflags "-linkmode external -extldflags -static -s" -o monitor_exporter
 cd ../transgateway
-go build -buildvcs=false  -ldflags "-linkmode external -extldflags -static -s"
+go build -ldflags "-linkmode external -extldflags -static -s"
 cd ../db_data_exporter
-go build -buildvcs=false  -ldflags "-linkmode external -extldflags -static -s"
+go build -ldflags "-linkmode external -extldflags -static -s"
 cd ../daemon_proc
 go build -o daemon_proc
 cd ../metric_comparison_exporter
-go build -buildvcs=false  -ldflags "-linkmode external -extldflags -static -s"
+go build -ldflags "-linkmode external -extldflags -static -s"
